90c9f60664781375fa0e982e2f4f4822c82f7dd8
Bombwhale <solkblte@icloud.com>
2024-02-08 17:54:40 +0900

    [front] Add styled-reset, styled-components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import reset from 'styled-reset'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 )
