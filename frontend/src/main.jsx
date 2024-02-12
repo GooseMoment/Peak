427ba7dc915fdbfd58d9f2a54c5c3dcd3e19f623
Bombwhale <solkblte@icloud.com>
2024-02-12 17:21:37 +0900

    Edit style: stretch html, add Pretendard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,20 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+
+    html {
+        height: 100vh;
+        width: 100vw;
+    }
+    
+    #root {
+        background-color: #FEFDFC;
+        height: 100%;
+    }
+
+    body, textarea {
+        font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif !important;
+    }
 `
 
 ReactDOM.createRoot(document.getElementById('root')).render(
