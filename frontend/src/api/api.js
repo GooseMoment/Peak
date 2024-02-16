03faf0467ad89bad918e55ad91732f5aba30901a
Bombwhale <solkblte@icloud.com>
2024-02-16 22:31:10 +0900

    Add api.js (api callback functions mockup)

---
+++ frontend/src/api/api.js
@@ -0,0 +1,147 @@
+const getCurrentUser = () => {
+}
+
+// Today
+
+const getTodayTasks = (userID) => {
+}
+
+// Projects
+
+const getProjectsList = (userID) => {
+
+}
+
+const getProject = (id) => {
+
+}
+
+const getTasksByDrawer = (drawerID) => {
+
+}
+
+const getTask = (id) => {
+
+}
+
+const postTask = (task) => {
+
+}
+
+const patchTask = (id, task) => {
+
+}
+
+const deleteTask = (id) => {
+
+}
+
+// TODO: complete task
+
+// Notifications
+
+const getNotifications = (userID, limit) => {
+
+}
+
+const getNotification = (id) => {
+
+}
+
+const deleteNotification = (id) => {
+
+}
+
+// Social
+
+// send (user: sender)
+// userID: receiver
+const postFollowRequest = (userID) => {
+
+}
+
+// accept or reject (user: receiver)
+// userID: sender
+const patchFollowRequest = (userID) => {
+
+}
+
+// cancel (user: sender)
+// userID: receiver
+const deleteFollowRequest = (userID) => {
+
+}
+
+const getProfile = (userID) => {
+
+}
+
+const getFollowers = (userID) => {
+
+}
+
+const getFollowings = (userID) => {
+
+}
+
+const getBlocks = () => {
+
+}
+
+const postBlock = (userID) => {
+
+}
+
+const deleteBlock = (userID) => {
+
+}
+
+const patchFollower = (userID) => {
+
+}
+
+const getDailyReport = (userID, date) => {
+
+}
+
+const getFollowingFeed = (date) => {
+
+}
+
+const getExploreFeed = (userID) => {
+
+}
+
+const getEmojis = () => {
+
+}
+
+const postReaction = (taskID, emoji) => {
+
+}
+
+const deleteReaction = (taskID) => {
+
+}
+
+const postCommentToTask = (taskID, comment) => {
+
+}
+
+const postCommentToDailyComment = (dailyCommentID, comment) => {
+
+}
+
+const postPeck = (taskID) => {
+
+}
+
+// Settings
+
+const patchUser = (user) => {
+
+}
+
+const getSettings = (userID) => {
+
+}
