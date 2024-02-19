ce18137534563d4d0c80f54451399b93cd2164bc
Bombwhale <solkblte@icloud.com>
2024-02-19 14:49:49 +0900

    Fix migration errors for Following and Block - Change one-to-one field to foreign_key - Add related_name for each column

--- backend/social/models.py
+++ backend/social/models.py
@@ -62,14 +62,16 @@ class Comment(models.Model):
 
 class Following(models.Model):
     # 보내는사람
-    follower = models.OneToOneField(
+    follower = models.ForeignKey(
         User,
         on_delete = models.CASCADE,
+        related_name = "follower"
     )
     # 받는사람
-    followee = models.OneToOneField(
+    followee = models.ForeignKey(
         User,
         on_delete = models.CASCADE,
+        related_name = "followee"
     )
     # 요청인가
     is_request = models.BooleanField(default=False)
@@ -80,11 +82,18 @@ class Following(models.Model):
         ]
 
 class Block(models.Model):
-    blocker = models.OneToOneField(
+    blocker = models.ForeignKey(
         User,
         on_delete = models.CASCADE,
+        related_name = "blocker"
     )
-    blockee = models.OneToOneField(
+    blockee = models.ForeignKey(
         User,
         on_delete = models.CASCADE,
+        related_name = "blockee"
     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["blocker", "blockee"], name="constraint_blocker_blockee"),
+        ]
