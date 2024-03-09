1a158f84220079d7c1c92050e23f2e4b27533be7
minyoy <minyoy@naver.com>
2024-03-09 17:07:18 +0900

    Create ProjectDetail, ProjectList at view.py

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,5 +1,40 @@
+from rest_framework import status, mixins, generics
+
+from .models import Project
+from .serializers import ProjectSerializer
+from rest_framework.decorators import APIView
+
 def get_projects_list(request, user_id):
     pass
 
 def get_project(request, id):
     pass
+
+class ProjectDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIview):
+    projects = Project.object.all()
+    serializer_class = ProjectSerializer
+    lookup_field = "id"
+
+    def get(self, request, id, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, id, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    
+class ProjectList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    projects = Project.object.all()
+    serializer_class = ProjectSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
