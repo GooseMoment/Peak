2324f2c23f2c839d00fea714bba41c15df0c1506
minyoy <minyoy@naver.com>
2024-03-11 21:33:26 +0900

    Add user at drawers

--- backend/drawers/models.py
+++ backend/drawers/models.py
@@ -3,10 +3,16 @@ from django.db import models
 import uuid
 
 from projects.models import Project
+from users.models import User
 
 class Drawer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="drawers",
+    )
     project = models.ForeignKey(
         Project,
         on_delete=models.CASCADE,
--- backend/drawers/serializers.py
+++ backend/drawers/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class DrawerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drawer
-        fields = ['id', 'name', 'project', 'order', 'privacy', 'uncompleted_task_count', 'completed_task_count', 'created_at', 'updated_at', 'deleted_at']
+        fields = ['id', 'name', 'user', 'project', 'order', 'privacy', 'uncompleted_task_count', 'completed_task_count', 'created_at', 'updated_at', 'deleted_at']
