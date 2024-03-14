441ae99f2c073e158584fe6ba362e1af277f4a44
Bombwhale <solkblte@icloud.com>
2024-03-14 12:32:59 +0900

    Edit backend settings.py - Add os.environ.get at SECRET_KEY, DEBUG, ALLOWED_HOST - Rename SECRET_KEY to DJANGO_SECRET_KEY

--- backend/django_peak/settings.py
+++ backend/django_peak/settings.py
@@ -20,16 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87%a)!2$$9_yizx8(%n%as513jq94o8(iuquctgpgxgic=+s7='
-# TODO: SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-87%a)!2$$9_yizx8(%n%as513jq94o8(iuquctgpgxgic=+s7=")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# TODO: DEBUG = False
+DEBUG = bool(os.environ.get("DEBUG", 1))
 
-ALLOWED_HOSTS = []
-# TODO: fill with hosts after setting DEBUG as False.
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
 
 # Application definition
 
