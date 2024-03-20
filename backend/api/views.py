048303d75b05324ca78c56248997c9ec0243e1af
minyoy <minyoy@naver.com>
2024-03-20 18:55:26 +0900

    Add CreateMixin

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import mixins, status
+from rest_framework.response import Response
 
-# Create your views here.
+class CreateMixin(mixins.CreateModelMixin):
+    def create_with_user(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        for k, v in kwargs:
+            serializer.validated_data[k] = v
+        serializer.validated_data["user"] = request.user
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
