d044786322005f771f9a511d53e980a73b6068db
Bombwhale <solkblte@icloud.com>
2024-03-22 23:28:15 +0900

    Remove unused import

--- frontend/src/components/sign/SignForm.jsx
+++ frontend/src/components/sign/SignForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { redirect, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 import Form from "@components/sign/Form"
 import Input from "@components/sign/Input"
