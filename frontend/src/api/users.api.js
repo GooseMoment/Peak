bc1d5a56e54b0dcad6b779740f6d6ce882e0087b
Bombwhale <solkblte@icloud.com>
2024-03-22 23:02:17 +0900

    Feature signing in, signing up (frontend) - Add @api/users/signUp - Add @utils/sleep

--- frontend/src/api/users.api.js
+++ frontend/src/api/users.api.js
@@ -33,3 +33,37 @@ export const signIn = async (email, password) => {
 
     return true
 }
+
+export const signUp = async (email, password, username) => {
+    try {
+        const res = await axios.post("sign_up/", {email, password, username})
+        return res
+    } catch (err) {
+        let msg = ""
+        if (err && err.code === "ERR_NETWORK") {
+            msg = "Please check your network."
+        } else if (err && err.response && err.response.status === 400) {
+            switch (err.response.data.code) {
+                case 'SIGNUP_SIGNED_IN_USER':
+                    msg = "You already signed in! Please go back to your app."
+                    break
+                case 'SIGNUP_USERNAME_TOO_SHORT':
+                    msg = "username should be more than 4 characters."
+                    break
+                case 'SIGNUP_PASSWORD_TOO_SHORT':
+                    msg = "You already signed in! Please go back to your app."
+                    break
+                case 'SIGNUP_USERNAME_WRONG':
+                    msg = "username should only contain alphabets, underscore, and digits."
+                    break
+            }
+        } else if (err && err.response && err.response.status === 500) {
+            msg = "Something's wrong with our server. Please try later."
+            return
+        } else {
+            throw err
+        }
+
+        throw new Error(msg)
+    }
+}
--- frontend/src/components/sign/SignForm.jsx
+++ frontend/src/components/sign/SignForm.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react"
+import { redirect, useNavigate } from "react-router-dom"
 
 import Form from "@components/sign/Form"
 import Input from "@components/sign/Input"
 import Button from "@components/sign/Button"
 
+import { signIn, signUp } from "@api/users.api"
+
+import notify from "@utils/notify"
+import sleep from "@utils/sleep"
+
 import styled from "styled-components"
 import {Mail, AtSign, Key} from "feather-icons-react"
+
 
 const SignForm = () => {
     const [active, setActive] = useState("signIn")
@@ -22,13 +29,39 @@ const SignInForm = ({setActive}) => {
         e.preventDefault()
         setActive("signUp")
     }
+    const [isLoading, setIsLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const onSubmit = async e => {
+        e.preventDefault()
+        setIsLoading(true)
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        try {
+            const res = await signIn(email, password)
+            if (res) {
+                notify.success("Login succeed. Welcome back!")
+                await sleep(1000)
+                navigate("/app")
+            } else {
+                notify.error("Login failed. Please check your email and password.")
+                setIsLoading(false)
+            }
+        } catch (err) {
+            notify.error("Please check your network.")
+            setIsLoading(false)
+            return
+        }
+    }
 
     return <Box>
         <Title>Sign in</Title>
-        <Form>
+        <Form onSubmit={onSubmit}>
             <Input icon={<Mail />} name="email" type="email" placeholder="id@example.com" required />
             <Input icon={<Key />} name="password" type="password" placeholder="********" required />
-            <Button type="submit">Go!</Button>
+            <Button type="submit" disabled={isLoading}>{isLoading ? "Loading" : "Go!"}</Button>
         </Form>
         <Links>
             <A href="#">Forgot password?</A>
@@ -42,10 +75,30 @@ const SignUpForm = ({setActive}) => {
         e.preventDefault()
         setActive("signIn")
     }
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmit = async e => {
+        e.preventDefault()
+        setIsLoading(true)
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+        const username = e.target.username.value
+
+        try {
+            await signUp(email, password, username)
+            notify.success("You become a new member! Please sign in.")
+            setActive("signIn")
+        } catch (err) {
+            notify.error("Sign up failed: " + err.message)
+            setIsLoading(false)
+            return
+        }
+    }
 
     return <Box>
         <Title>Sign in</Title>
-        <Form>
+        <Form onSubmit={onSubmit}>
             <Input icon={<Mail />} name="email" type="email" placeholder="id@example.com" required />
             <Input icon={<Key />} name="password" type="password" placeholder="********" required />
             <Input icon={<AtSign />} name="username" type="text" 
@@ -53,7 +106,7 @@ const SignUpForm = ({setActive}) => {
             <TosAgreement>
                 By creating an account, you agree to our <a href="/tos">Terms of Service</a>.
             </TosAgreement>
-            <Button type="submit">Let's start!</Button>
+            <Button type="submit" disabled={isLoading}>{isLoading ? "Loading" : "Let's start!"}</Button>
         </Form>
         <Links>
             <A href="#" onClick={goToSignIn}>Already have an account</A>
