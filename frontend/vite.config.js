ac81b7d90045ec7280877ae2b8c027440812a65c
Bombwhale <solkblte@icloud.com>
2024-03-23 16:49:15 +0900

    Install vite-plugin-pwa - Edit vite.config.js to apply VitePWA plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import path from "path"
+
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +11,19 @@ export default defineConfig({
         port: 3000,
         host: "0.0.0.0",
     },
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            injectRegister: "script-defer",
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
+    ],
     resolve: {
         alias: [
             { find: '@', replacement: path.resolve(__dirname, 'src') },
