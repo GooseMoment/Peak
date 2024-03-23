a9412e877a81381b542d9e97a4b7c79710fba47f
Bombwhale <solkblte@icloud.com>
2024-03-24 00:28:27 +0900

    Add webmanifest, and icons - Install vite-pwa/assets-generator - Generate icons by assets-generator

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,6 +22,35 @@ export default defineConfig({
             devOptions: {
                 enabled: true,
             },
+            manifest: {
+                name: 'Peak',
+                short_name: 'Peak',
+                description: 'Pick, Peek, Peck, and Peak.',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        "src": "pwa-64x64.png",
+                        "sizes": "64x64",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "pwa-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "pwa-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "maskable-icon-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    }
+                ]
+            },
         }),
     ],
     resolve: {
