0486b20d1dd8819ad989fe90545d8438f2981015
Bombwhale <solkblte@icloud.com>
2024-03-23 16:25:07 +0900

    Add console.log(error) in ErrorPage.jsx

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,14 +1,16 @@
 import { Link, useRouteError } from "react-router-dom"
 
-import Layout from "@/containers/Layout"
+import Layout from "@containers/Layout"
 
 const ErrorPage = () => {
     const error = useRouteError();
 
+    console.log(error)
+
     return <Layout noSidebar={true}>
         <h1>{error.status}: {error.statusText}</h1>
-        <p>To GooseMoment Dev, if you encouter this error, please check <code>@/router.jsx</code>.</p>
-        <Link to="/">Go to index</Link>
+        <p>브라우저 콘솔 확인하세요</p>
+        <Link to="">Go to index</Link>
     </Layout>
 }
 
