b7340b7cc71903da95c61b1e6bbd72e9c5c93b5b
minyoy <minyoy@naver.com>
2024-03-26 21:01:12 +0900

    Delete read_only=True at DrawerSerializer's (un)completed_task_count

--- backend/drawers/serializers.py
+++ backend/drawers/serializers.py
@@ -6,8 +6,8 @@ from users.models import User
 class DrawerSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(required=False, queryset=User.objects.all())
     order = serializers.IntegerField(min_value=0, required=False)
-    uncompleted_task_count = serializers.IntegerField(read_only=True, default=0, required=False)
-    completed_task_count = serializers.IntegerField(read_only=True, default=0, required=False)
+    uncompleted_task_count = serializers.IntegerField(default=0, required=False)
+    completed_task_count = serializers.IntegerField(default=0, required=False)
     
     class Meta:
         model = Drawer
