2e33cd7b952646828b3aaae5d6caa1f5d521bddf
minyoy <minyoy@naver.com>
2024-03-26 21:00:31 +0900

    Add get_client_ip at api/views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework import mixins, status
 from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.decorators import api_view
 
 class CreateMixin(mixins.CreateModelMixin):
     def create_with_user(self, request, *args, **kwargs):
@@ -11,3 +13,16 @@ class CreateMixin(mixins.CreateModelMixin):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+# from https://stackoverflow.com/questions/4581789/how-do-i-get-user-ip-address-in-django ㅎㅎ
+def get_client_ip(request: Request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+@api_view(["GET"])
+def get_healthcheck(request: Request):
+    return Response({"data": "success", "ip": get_client_ip(request)})
