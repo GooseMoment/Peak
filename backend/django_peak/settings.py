ae278edc51d6a89ffa436603719020922273ca69
minyoy <minyoy@naver.com>
2024-03-26 21:02:40 +0900

    Fix error

--- backend/django_peak/settings.py
+++ backend/django_peak/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-87%a)!2$$9_yiz
 
 DEBUG = os.environ.get("DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost:8000 localhost localhost:8080 127.0.0.1:8000 127.0.0.1").split(" ")
 
 # Application definition
 
--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -5,6 +5,8 @@ import Layout from "@/containers/Layout"
 const ErrorPage = () => {
     const error = useRouteError();
 
+    console.log(error)
+
     return <Layout noSidebar={true}>
         <h1>{error.status}: {error.statusText}</h1>
         <p>To GooseMoment Dev, if you encouter this error, please check <code>@/router.jsx</code>.</p>
