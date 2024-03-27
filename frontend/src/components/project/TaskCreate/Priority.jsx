8f044251080853565cd236c74b2c59e2fef6c9aa
minyoy <minyoy@naver.com>
2024-03-27 17:57:17 +0900

    Add svg at priority

--- frontend/src/components/project/TaskCreate/Priority.jsx
+++ frontend/src/components/project/TaskCreate/Priority.jsx
@@ -2,12 +2,16 @@ import FeatherIcon from "feather-icons-react"
 import styled from "styled-components"
 import DetailFrame from "./DetailFrame"
 
+import normal from "@/assets/project/priority/normal.svg"
+import important from "@/assets/project/priority/important.svg"
+import critical from "@/assets/project/priority/critical.svg"
+
 const Priority = ({ onClose }) => {
     return (
         <DetailFrame title="중요도 설정" onClose={onClose}>
             {items.map(item => (
                 <ItemBlock key={item.id}>
-                    <FeatherIcon icon={item.icon} />
+                    {item.icon}
                     <ItemText>{item.content}</ItemText>
                 </ItemBlock>
             ))}
@@ -17,20 +21,16 @@ const Priority = ({ onClose }) => {
 
 const ItemBlock = styled.div`
     display: flex;
-    justify-content: flex-start;
+    gap: 0.7em;
+    align-items: center;
     margin-left: 1.2em;
-
-    & svg {
-        stroke: #FF4A03;
-        top: 1.2em;
-    }
+    margin-top: 1.2em;
 `
 
 const ItemText = styled.p`
     font-weight: normal;
     font-size: 1em;
     color: #000000;
-    margin-top: 1.3em;
 
     &:hover {
         font-weight: bolder;
@@ -40,9 +40,9 @@ const ItemText = styled.p`
 `
 
 const items = [
-    {id: 0, icon: "alert-circle", content: "보통"},
-    {id: 1, icon: "alert-circle", content: "중요"},
-    {id: 2, icon: "alert-circle", content: "매우 중요"},
+    {id: 0, icon: <img src={normal}/>, content: "보통"},
+    {id: 1, icon: <img src={important}/>, content: "중요"},
+    {id: 2, icon: <img src={critical}/>, content: "매우 중요"},
 ]
 
 export default Priority
