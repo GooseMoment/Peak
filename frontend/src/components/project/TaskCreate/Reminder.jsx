c03fa1855048606972dcd08216cede851362981e
minyoy <minyoy@naver.com>
2024-03-27 19:26:24 +0900

    Add svg at Reminder

--- frontend/src/components/project/TaskCreate/Reminder.jsx
+++ frontend/src/components/project/TaskCreate/Reminder.jsx
@@ -3,12 +3,21 @@ import styled from "styled-components"
 
 import DetailFrame from "./DetailFrame"
 
+import before_5 from "@/assets/project/reminder/before_5.svg"
+import before_15 from "@/assets/project/reminder/before_15.svg"
+import before_30 from "@/assets/project/reminder/before_30.svg"
+import before_1h from "@/assets/project/reminder/before_1h.svg"
+import before_D from "@/assets/project/reminder/before_D.svg"
+import before_1D from "@/assets/project/reminder/before_1D.svg"
+import before_2D from "@/assets/project/reminder/before_2D.svg"
+import custom from "@/assets/project/reminder/custom.svg"
+
 const Reminder = ({ onClose }) => {
     return (
         <DetailFrame title="알람 설정" onClose={onClose}>
             {items.map(item => (
                 <ItemBlock key={item.id}>
-                    <FeatherIcon icon={item.icon} />
+                    {item.icon}
                     <ItemText>{item.content}</ItemText>
                 </ItemBlock>
             ))}
@@ -18,8 +27,10 @@ const Reminder = ({ onClose }) => {
 
 const ItemBlock = styled.div`
     display: flex;
-    justify-content: flex-start;
+    gap: 0.5em;
+    align-items: center;
     margin-left: 1.2em;
+    margin-top: 1.2em;
 
     & svg {
         stroke: #FF4A03;
@@ -31,7 +42,6 @@ const ItemText = styled.p`
     font-weight: normal;
     font-size: 1em;
     color: #000000;
-    margin-top: 1.3em;
 
     &:hover {
         font-weight: bolder;
@@ -41,14 +51,14 @@ const ItemText = styled.p`
 `
 
 const items = [
-    {id: 0, icon: "clock", content: "5분 전"},
-    {id: 1, icon: "clock", content: "15분 전"},
-    {id: 2, icon: "clock", content: "30분 전"},
-    {id: 3, icon: "clock", content: "1시간 전"},
-    {id: 4, icon: "clock", content: "당일"},
-    {id: 5, icon: "clock", content: "1일 전"},
-    {id: 6, icon: "clock", content: "2일 전"},
-    {id: 7, icon: "clock", content: "사용자 지정"},
+    {id: 0, icon: <img src={before_5}/>, content: "5분 전"},
+    {id: 1, icon: <img src={before_15}/>, content: "15분 전"},
+    {id: 2, icon: <img src={before_30}/>, content: "30분 전"},
+    {id: 3, icon: <img src={before_1h}/>, content: "1시간 전"},
+    {id: 4, icon: <img src={before_D}/>, content: "당일"},
+    {id: 5, icon: <img src={before_1D}/>, content: "1일 전"},
+    {id: 6, icon: <img src={before_2D}/>, content: "2일 전"},
+    {id: 7, icon: <img src={custom}/>, content: "사용자 지정"},
 ]
 
 export default Reminder
