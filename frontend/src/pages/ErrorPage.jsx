8587d89d2bcd034adc3bae1a459e66d9ae401ad5
Bombwhale <solkblte@icloud.com>
2024-03-27 17:34:07 +0900

    Add check for isRouteErrorResponse

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,15 +1,24 @@
-import { Link, useRouteError } from "react-router-dom"
+import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom"
 
 import Layout from "@containers/Layout"
+import PageTitle from "@components/common/PageTitle"
 
 const ErrorPage = () => {
     const error = useRouteError();
 
+    if (isRouteErrorResponse(error)) {
+        return <Layout>
+            <PageTitle>{error.status}: {error.statusText}</PageTitle>
+            <p>라우터 에러!</p>
+            <Link to="">Go to index</Link>
+        </Layout>
+    }
+
     console.log(error)
 
     return <Layout noSidebar={true}>
-        <h1>{error.status}: {error.statusText}</h1>
-        <p>브라우저 콘솔 확인하세요</p>
+        <PageTitle>이런!</PageTitle>
+        <p>라우터 외의 에러입니다. 콘솔 확인하세요.</p>
         <Link to="">Go to index</Link>
     </Layout>
 }
