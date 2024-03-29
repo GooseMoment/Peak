d82edd95f09006a614b7354f9860a189d3a2e280
minyoy <minyoy@naver.com>
2024-03-29 14:53:38 +0900

    Add drawer_name and project_name at TaskSerializer

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -5,7 +5,18 @@ from users.models import User
 
 class TaskSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(required=False, queryset=User.objects.all())
+    drawer_name = serializers.SerializerMethodField(read_only=True)
+    project_name = serializers.SerializerMethodField(read_only=True)
 
+    def get_drawer_name(self, obj):
+        return obj.drawer.name
+
+    def get_project_name(self, obj):
+        return obj.drawer.project.name
+    
     class Meta:
         model = Task
-        fields = ['id', 'name', 'privacy', 'completed_at', 'drawer', 'due_date', 'due_time', 'priority', 'memo', 'reminder_datetime', 'user', 'repeat', 'created_at', 'updated_at', 'deleted_at']
+        fields = [
+            'id', 'name', 'privacy', 'completed_at', 'drawer', 'drawer_name', 'due_date', 'due_time', 'priority', 'memo', 
+            'reminder_datetime', 'user', 'repeat', 'created_at', 'updated_at', 'deleted_at', 'project_name',
+        ]
