b63fa1280a8284a68b192f89cc08bdebdf536a47
Bombwhale <solkblte@icloud.com>
2024-03-31 01:02:22 +0900

    Add serializers, views, admin for UserSetting - Edit users/urls.py to attach UserSettingDetail

--- backend/user_setting/views.py
+++ backend/user_setting/views.py
@@ -1,1 +1,21 @@
-from django.http import HttpRequest
+from rest_framework import mixins, generics, permissions
+
+from .models import UserSetting
+from .serializers import UserSettingSerializer
+from api.permissions import IsUserMatch
+
+class UserSettingDetail(mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    generics.GenericAPIView):
+    
+    queryset = UserSetting.objects.all()
+    serializer_class = UserSettingSerializer
+    lookup_field = "user__username"
+    lookup_url_kwarg = "username"
+    permission_classes = [IsUserMatch]
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from user_setting.views import UserSettingDetail
 
 urlpatterns = [
     path("users/me/", views.get_me),
@@ -8,4 +9,5 @@ urlpatterns = [
     path("sign_out/", views.sign_out),
     path("sign_up/", views.sign_up),
     path("users/@<str:username>/", views.UserDetail.as_view()),
+    path("users/@<str:username>/setting/", UserSettingDetail.as_view())
 ]
