27fef27e096818777a11a8040e2a58f803ed4ffb
Bombwhale <solkblte@icloud.com>
2024-03-31 00:24:02 +0900

    Makemigrations for user_setting - Add fields for UserSetting - Add user_setting to settings.py

--- backend/user_setting/models.py
+++ backend/user_setting/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
-from api.models import Base
+from api.models import Base, PrivacyMixin
 from users.models import User
+from social.models import Emoji
 
 class UserSetting(Base):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        related_name="setting",
     )
     follow_request_approval_manually = models.BooleanField(default=False)
-    # TODO: 만들고 추가하세요 구영서씨
+    follow_request_approval_for_followings = models.BooleanField(default=True)
+    follow_list_privacy = models.CharField(max_length=128, choices=PrivacyMixin.PRIVACY_TYPES)
+    favorite_emojis = models.ManyToManyField(
+        to=Emoji,
+        related_name="favorites",
+    )
+    dislikable_emojis = models.ManyToManyField(
+        to=Emoji,
+        related_name="dislikables"
+    )
