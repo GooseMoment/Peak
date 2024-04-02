198ba224a8655d9bc204e751f2286233b1a9b343
minyoy <minyoy@naver.com>
2024-04-02 18:55:13 +0900

    Add api at DrawerCreate and ProjectCreate

--- frontend/src/components/project/Creates/DrawerCreate.jsx
+++ frontend/src/components/project/Creates/DrawerCreate.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react"
+import { useRevalidator, useParams } from "react-router-dom"
 
 import styled from "styled-components"
 
 import { cubicBeizer } from "@assets/keyframes"
-import TitleFrame from "@components/project/common/TitleFrame"
-import MiddleFrame from "@components/project/common/MiddleFrame"
+import TitleFrame from "@components/project/common/Title"
+import MiddleFrame from "@components/project/common/Middle"
 import Privacy from "./Privacy"
 
+import { postDrawer } from "@api/drawers.api"
+import notify from "@utils/notify"
+
 const DrawerCreate = ({onClose}) => {
+    const { id } = useParams()
+
+    const revalidator = useRevalidator()
+
+    const [name, setName] = useState('')
+    const [privacy, setPrivacy] = useState('')
+    const [displayPrivacy, setDisplayPrivacy] = useState('')
+
+    //Component
     const [isComponentOpen, setIsComponentOpen] = useState(false)
 
     const closeComponent = () => {
@@ -15,25 +28,51 @@ const DrawerCreate = ({onClose}) => {
     }
 
     const items = [
-        {icon: "server", display: "전체공개", component: <Privacy closeComponent={closeComponent}/>},
+        {icon: "server", display: displayPrivacy, component: <Privacy setPrivacy={setPrivacy} setDisplayPrivacy={setDisplayPrivacy} closeComponent={closeComponent}/>},
     ]
+
+    const makeDrawer = async (name, privacy) => {
+        try {
+            const edit = {
+                'project': id,
+                'name': name,
+                'privacy': privacy,
+            }
+            await postDrawer(edit)
+            notify.success("서랍 생성에 성공하였습니다.")
+        } catch (e) {
+            notify.err("서랍 생성에 실패했습니다.")
+        }
+    }
+
+    const submit = async (e) => {
+        await makeDrawer(name, privacy)
+        onClose()
+        revalidator.revalidate()
+    }
 
     return (
         <DrawerBox>
-            <TitleFrame title="수강신청" icon="inbox" onClose={onClose}/>
-            <MiddleFrame items={items} isComponentOpen={isComponentOpen} setIsComponentOpen={setIsComponentOpen}/>
+            <TitleFrame name={name} setName={setName} icon="inbox" onClose={onClose}/>
+            <MiddleFrame items={items} submit={submit} isComponentOpen={isComponentOpen} setIsComponentOpen={setIsComponentOpen}/>
         </DrawerBox>
     )
 }
 
 const DrawerBox = styled.div`
     width: 35em;
-    height: 8em;
     background-color: #FFFFFF;
     border: solid 1px #D9D9D9;
     border-radius: 15px;
 
     transition: left 0.5s ${cubicBeizer}, width 0.5s ${cubicBeizer};
+
+    &::after {
+        content: " ";
+        display: block;
+        height: 0;
+        clear: both;
+    }
 `
 
 export default DrawerCreate
--- frontend/src/components/project/Creates/ProjectCreate.jsx
+++ frontend/src/components/project/Creates/ProjectCreate.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react"
+import { useRevalidator } from "react-router-dom"
 
 import styled from "styled-components"
 
 import { cubicBeizer } from "@assets/keyframes"
-import TitleFrame from "@components/project/common/TitleFrame"
-import MiddleFrame from "@components/project/common/MiddleFrame"
+import TitleFrame from "@components/project/common/Title"
+import MiddleFrame from "@components/project/common/Middle"
+import notify from "@utils/notify"
 import Color from "./Color"
 import Type from "./Type"
 
+import { postProject } from "@api/projects.api"
+
 const ProjectCreate = ({onClose}) => {
+    const revalidator = useRevalidator()
+
+    const [name, setName] = useState('')
+    const [color, setColor] = useState('')
+    const [displayColor, setDisplayColor] = useState('')
+    const [type, setType] = useState('')
+    const [displayType, setDisplayType] = useState('')
+
+     //Component
     const [isComponentOpen, setIsComponentOpen] = useState(false)
 
     const closeComponent = () => {
@@ -16,15 +29,35 @@ const ProjectCreate = ({onClose}) => {
     }
 
     const items = [
-        {icon: "circle", color: 'FD99D9', display: "파랑", component: <Color closeComponent={closeComponent}/>},
-        {icon: "award", display: "상시 프로젝트", component: <Type closeComponent={closeComponent}/>}
+        {icon: "circle", color: color, display: displayColor, component: <Color setColor={setColor} setDisplayColor={setDisplayColor} closeComponent={closeComponent}/>},
+        {icon: "award", display: displayType, component: <Type setType={setType} setDisplayType={setDisplayType} closeComponent={closeComponent}/>}
     ]
+
+    const makeProject = async (name, color, type) => {
+        try {
+            const edit = {
+                'name': name,
+                'color': color,
+                'type': type,
+            }
+            await postProject(edit)
+            notify.success("프로젝트 생성에 성공하였습니다.")
+        } catch (e) {
+            notify.error("프로젝트 생성에 실패했습니다.")
+        }
+    }
+
+    const submit = async (e) => {
+        await makeProject(name, color, type)
+        onClose()
+        revalidator.revalidate()
+    }
 
     return (
         <>
         <ProjectBox>
-            <TitleFrame title="홍대라이프" icon="archive" onClose={onClose}/>
-            <MiddleFrame items={items} isComponentOpen={isComponentOpen} setIsComponentOpen={setIsComponentOpen}/>
+            <TitleFrame name={name} setName={setName} icon="archive" onClose={onClose}/>
+            <MiddleFrame items={items} submit={submit} isComponentOpen={isComponentOpen} setIsComponentOpen={setIsComponentOpen}/>
         </ProjectBox>
         </>
     )
@@ -32,12 +65,18 @@ const ProjectCreate = ({onClose}) => {
 
 const ProjectBox = styled.div`
     width: 35em;
-    height: 11em;
     background-color: #FFFFFF;
     border: solid 1px #D9D9D9;
     border-radius: 15px;
 
     transition: left 0.5s ${cubicBeizer}, width 0.5s ${cubicBeizer};
+    
+    &::after {
+        content: " ";
+        display: block;
+        height: 0;
+        clear: both;
+    }
 `
 
 export default ProjectCreate
