d5f2fcfac04e2d9a43183910ac799a82de8000da
minyoy <minyoy@naver.com>
2024-04-02 18:54:04 +0900

    Create ProjectListPage

--- frontend/src/pages/ProjectListPage.jsx
+++ frontend/src/pages/ProjectListPage.jsx
@@ -1,11 +1,11 @@
-import { useRouteLoaderData } from "react-router-dom"
+import { Link, useRouteLoaderData } from "react-router-dom"
 import { useState } from "react"
 
 import styled from "styled-components"
 import FeatherIcon from "feather-icons-react"
 
 import ModalPortal from "@components/common/ModalPortal"
-import ProjectCreate from "@/components/project/Creates/ProjectCreate"
+import ProjectCreate from "@components/project/creates/ProjectCreate"
 
 const ProjectListPage = () => {
     const {projects} = useRouteLoaderData("app")
@@ -13,11 +13,20 @@ const ProjectListPage = () => {
 
     return(
         <>
+            <TitleText>프로젝트</TitleText>
             {projects && projects.map((project) => (
-                <FlexBox>
-                    <FeatherIcon icon="circle" fill={`#`+ project.color}/>
-                    <div>{project.name}</div>
-                </FlexBox>
+                <Box key={project.id}>
+                    <FlexBox>
+                        <FeatherIcon icon="circle" fill={`#`+ project.color}/>
+                        <Link to={`/app/projects/${project.id}`} style={{ textDecoration: 'none' }}>
+                            <NameText>{project.name}</NameText>
+                        </Link>
+                        <TypeText>{project.type === 'regular' ? "상시" : "목표"}</TypeText>
+                    </FlexBox>
+                    <FlexBox>
+                        <Text>완수한 일 : 30 / 완수하지 못한 일 : 40</Text>
+                    </FlexBox>
+                </Box>
             ))}
             <TaskCreateButton onClick={() => {setsIsCreateOpen(true)}}>
                 <FeatherIcon icon="plus-circle"/>
@@ -31,15 +40,61 @@ const ProjectListPage = () => {
     )
 }
 
+const TitleText = styled.div`
+    font-size: 2em;
+    font-weight: bolder;
+    margin-bottom: 1em;
+    color: #000000;
+`
+
+const Box = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
 const FlexBox = styled.div`
     display: flex;
+    align-items: center;
+    margin: 1em 0.5em;
+
+    & svg {
+        width: 1.5em;
+        height: 1.5em;
+        stroke: none;
+        top: 0;
+    }
+`
+
+const NameText = styled.div`
+    font-weight: normal;
+    font-size: 1.25em;
+    margin-left: 0.3em;
+    color: #000000;
+
+    &:hover {
+        color: #FF4A03;
+        cursor: pointer;
+    }
+`
+
+const TypeText = styled.div`
+    font-weight: normal;
+    font-size: 1em;
+    margin-left: 0.6em;
+    color: #6E6E6E;
+`
+
+const Text = styled.div`
+    font-size: 1em;
+    color: #000000;
 `
 
 const TaskCreateButton = styled.div`
     flex: 1;
     display: flex;
     align-items: center;
-    margin-left: 1.3em;
+    margin-left: 1.2em;
     margin-top: 1.3em;
     
     &:hover {
