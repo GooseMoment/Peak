c177dc6c7c30ab0cb71bf5bdc3c13000674791eb
Bombwhale <solkblte@icloud.com>
2024-04-03 02:23:12 +0900

    Fix UserSerializer profile_img encoding problem

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,10 +2,17 @@ from rest_framework import serializers
 
 from .models import User
 
+class DefaultImageField(serializers.ImageField):
+    def to_representation(self, value):
+        if not value:
+            return "https://assets-dev.peak.ooo/user_profile_imgs%2Fdefault.jpg"
+        
+        return super().to_representation(value)
+
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.SerializerMethodField(read_only=True)
     is_me = serializers.SerializerMethodField(read_only=True)
-    profile_img = serializers.SerializerMethodField(read_only=True)
+    profile_img = DefaultImageField(read_only=True)
 
     class Meta:
         model = User
@@ -22,10 +29,4 @@ class UserSerializer(serializers.ModelSerializer):
     
     def get_is_me(self, obj):
         return self.context.get("is_me", False)
-    
-    def get_profile_img(self, obj):
-        if obj.profile_img:
-            return obj.profile_img
-        
-        return "https://assets-dev.peak.ooo/user_profile_imgs%2Fdefault.jpg"
     
