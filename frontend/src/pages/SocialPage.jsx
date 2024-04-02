f51b3e41ca194d4ebbf8454c04e06fa0791fe793
andless <andless2004@gmail.com>
2024-04-02 21:03:00 +0900

    Edti SocialPage to return null

--- frontend/src/pages/SocialPage.jsx
+++ frontend/src/pages/SocialPage.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 
-const SocialPage =()=> {
+const SocialPage = () => {
     const navigate = useNavigate()
 
     useEffect(()=> {
         navigate("social/following")
     })
+
+    return null
 }
 
 export default SocialPage
