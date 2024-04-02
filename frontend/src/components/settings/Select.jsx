51bb77e11dc0528da1285eb5a43acfc5575a525f
Bombwhale <solkblte@icloud.com>
2024-04-02 20:05:11 +0900

    Style Select in settings

--- frontend/src/components/settings/Select.jsx
+++ frontend/src/components/settings/Select.jsx
@@ -27,8 +27,42 @@ const Select = ({settings=getClientSettings(), name, submit, choices, online=fal
     </StyledSelect>
 }
 
+// from: https://codepen.io/vkjgr/pen/VYMeXp
 const StyledSelect = styled.select`
-    
+    background-color: white;
+    border: thin solid black;
+    border-radius: 4px;
+    display: inline-block;
+    font: inherit;
+    line-height: 1.5em;
+    padding: 0.5em 3.5em 0.5em 1em;
+
+    margin: 0;      
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+
+    background-image:
+        linear-gradient(45deg, transparent 50%, gray 50%),
+        linear-gradient(135deg, gray 50%, transparent 50%),
+        linear-gradient(to right, #ccc, #ccc);
+    background-position:
+        calc(100% - 20px) calc(1em + 2px),
+        calc(100% - 15px) calc(1em + 2px),
+        calc(100% - 2.5em) 0.5em;
+    background-size:
+        5px 5px,
+        5px 5px,
+        1px 1.5em;
+    background-repeat: no-repeat;
+
+    &:focus {
+        background-repeat: no-repeat;
+        border-color: green;
+        outline: 0;
+    }
 `
 
 export default Select
