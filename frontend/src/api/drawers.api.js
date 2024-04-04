db0adc2de0b5b185f232c9fca52e67ea30c8c5d2
minyoy <minyoy@naver.com>
2024-04-05 02:07:55 +0900

    Edit ./client->@api/client

--- frontend/src/api/drawers.api.js
+++ frontend/src/api/drawers.api.js
@@ -1,4 +1,4 @@
-import client from "./client"
+import client from "@api/client"
 
 export const getDrawersByProject = async (project_id) => {
     try {
--- frontend/src/api/tasks.api.js
+++ frontend/src/api/tasks.api.js
@@ -1,4 +1,4 @@
-import client from "./client"
+import client from "@api/client"
 
 export const getTasksByDrawer = async (drawerID) => {
     try {
