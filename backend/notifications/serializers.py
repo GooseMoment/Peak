3d4bbb0a506030536c5f39708734ff394431b386
Bombwhale <solkblte@icloud.com>
2024-04-06 01:59:32 +0900

    Fill NotificationSerializer - Add TaskReminderSerializer

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -1,14 +1,24 @@
-from .models import Notification
+from .models import TaskReminder, Notification
 from rest_framework import serializers
 
+from tasks.serializers import TaskSerializer
+from users.serializers import UserSerializer
+from social.serializers import ReactionSerializer, FollowingSerializer, PeckSerializer
+
+class TaskReminderSerializer(serializers.ModelSerializer):
+    task = TaskSerializer(read_only=True)
+     
+    class Meta:
+        model = TaskReminder
+        fields = ["task", "scheduled"]
+
 class NotificatonSerializer(serializers.ModelSerializer):
-    # TODO: add nested relationships
-    # https://www.django-rest-framework.org/api-guide/relations/#example
-    # user = 
-    # task = 
-    # reaction = 
-    # follow_request = 
+    user = UserSerializer()
+    task_reminder = TaskReminderSerializer()
+    reaction = ReactionSerializer()
+    following = FollowingSerializer()
+    peck = PeckSerializer()
 
     class Meta:
         model = Notification
-        fields = ["id", "type", "user", "task", "reaction", "follow_request"]
+        fields = ["id", "type", "user", "task_reminder", "reaction", "following", "peck"]
