f2a23a0364286a72fa2d21cc89c3c957c8559fa4
Bombwhale <solkblte@icloud.com>
2024-04-05 11:54:55 +0900

    Fix merge conflict

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
 import GlobalStyle from '@assets/GlobalStyle'
 
-<<<<<<< HEAD
 import { initClientSettings } from '@utils/clientSettings'
 
 import { ToastContainer } from "react-toastify"
@@ -13,10 +12,6 @@ import "react-toastify/dist/ReactToastify.css"
 
 // initilize client-side settings
 initClientSettings()
-=======
-import { ToastContainer } from 'react-toastify'
-import "react-toastify/dist/ReactToastify.css"
->>>>>>> 2ceba5dca70d57cbfd4a78273c4d744a13b09634
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
