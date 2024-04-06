f7efb0a9e7fcbbc60a608f64b7738aca1f81f6a9
minyoy <minyoy@naver.com>
2024-04-06 17:53:56 +0900

    Edit pagination size 10 -> 1000

--- backend/django_peak/settings.py
+++ backend/django_peak/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 1000
 }
 
 MIDDLEWARE = [
