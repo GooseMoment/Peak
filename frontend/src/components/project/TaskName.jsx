71862d83be7c17fd0d654fe53d301b589437f324
minyoy <minyoy@naver.com>
2024-04-07 01:50:01 +0900

    Fix toComplete Error AND Add ChangeTaskName

--- frontend/src/components/project/TaskName.jsx
+++ frontend/src/components/project/TaskName.jsx
@@ -1,4 +1,4 @@
-import { Link, useSubmit } from "react-router-dom"
+import { Link, useSubmit, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 import styled, {keyframes, css} from "styled-components"
@@ -7,6 +7,7 @@ import FeatherIcon from "feather-icons-react"
 function TaskName({projectId, task, color, editable}){
     const date = new Date()
     const submit = useSubmit()
+    const navigate = useNavigate()
     const pathRoot = `/app/projects/${projectId}/tasks/${task.id}/detail`
 
     const [taskName, setTaskName] = useState(task.name)
@@ -19,6 +20,17 @@ function TaskName({projectId, task, color, editable}){
     const onchange = (e) => {
         setTaskName(e.target.value)
     }
+    
+    const changeTaskName = async (name) => {
+        submit({name}, {method: "PATCH"})
+    }
+
+    const onEnter = async (e) => {
+        if(e.key === 'Enter') {
+            changeTaskName(taskName)
+            navigate(`/app/projects/${projectId}`)
+        }
+    }
 
     const toComplete = () => {
         setIsLoading(true)
@@ -26,7 +38,11 @@ function TaskName({projectId, task, color, editable}){
         if (!(task.completed_at)) {
             completed_at = date.toISOString()
         }
-        submit({id: task.id, completed_at}, {method: "PATCH"})
+        submit({id: task.id, completed_at}, {
+            method: "PATCH",
+            action: `/app/projects/${projectId}`,
+            navigate: false,
+        })
     }
 
     const EditView = (
@@ -35,6 +51,7 @@ function TaskName({projectId, task, color, editable}){
             type='text'
             onChange={onchange}
             value={taskName}
+            onKeyDown={onEnter}
             placeholder="할 일의 이름을 입력해주세요."
         />
     )
@@ -52,6 +69,7 @@ function TaskName({projectId, task, color, editable}){
                     $completed={task.completed_at ? true : false}
                     $color={color}
                     $due_date={task.due_date}
+                    $editable={editable}
                     $isLoading={isLoading}
                     onClick={toComplete}
                 >
@@ -92,7 +110,7 @@ const reverseRotateAnimation = keyframes`
 const TaskCircle = styled.div`
     display: flex;
     justify-content: center;
-    top: ${(props) => (props.$due_date ? 0.3 : 0)}em;
+    top: ${(props) => (props.$editable ? 0.1 : props.$due_date ? 0.3 : 0)}em;
     width: 1.2em;
     height: 1.2em;
     border-radius: 50%;
@@ -121,12 +139,17 @@ const TaskCircle = styled.div`
 `
 
 const Text = styled.div`
+    width: 60em;
     font-style: normal;
     font-size: 1.1em;
     color: ${(props) => (props.$completed ? '#A4A4A4' : '#000000')};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 `
 
 const InputText = styled.input`
+    width: 36em;
     font-weight: normal;
     font-size: 1.1em;
     color: ${(props) => (props.$completed ? '#A4A4A4' : '#000000')};
