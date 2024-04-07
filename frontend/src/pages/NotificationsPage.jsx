aabe1eaa4601b9dc1fe537977156f261d863a57d
Bombwhale <solkblte@icloud.com>
2024-04-07 23:50:02 +0900

    Add infinite scroll in NotificationPage

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -3,11 +3,11 @@ import { Fragment, useState } from "react"
 import FilterButtonGroup from "@components/notifications/FilterButtonGroup"
 import Box from "@components/notifications/Box"
 import PageTitle from "@components/common/PageTitle"
-import Button from "@components/sign/Button"
 
 import { getNotifications } from "@api/notifications.api"
 
 import { useInfiniteQuery } from "@tanstack/react-query"
+import { ImpressionArea } from "@toss/impression-area"
 
 const getCursorFromURL = (url) => {
     if (!url) return null
@@ -35,8 +35,6 @@ const NotificationsPage = () => {
         <FilterButtonGroup filters={filters} active={activeFilter} setActive={setActiveFilter} />
     </>
 
-    // TODO: add skeleton
-
     if (isError) {
         console.log("error:", error)
         return <div>Error!</div>
@@ -44,13 +42,17 @@ const NotificationsPage = () => {
 
     return <>
     {header}
+    {
+        isFetching && !isFetchingNextPage ? <div>TODO: add skeleton</div> : null
+    }
     {data?.pages.map((group, i) => (
         <Fragment key={i}>
             {group.results.map(notification => <Box key={notification.id} notification={notification} />)}
         </Fragment>
     ))}
-    <Button onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>Load more</Button>
-    {isFetchingNextPage && <div>Fetching next page...</div>}
+    <ImpressionArea onImpressionStart={() => fetchNextPage()} timeThreshold={200}>
+        {hasNextPage ? "Loading more..." : "No more notifications!"}
+    </ImpressionArea>
     </>
 }
 
