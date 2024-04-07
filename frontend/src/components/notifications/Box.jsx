16c011f477d85a1050f90303ac02e35776373e8e
Bombwhale <solkblte@icloud.com>
2024-04-08 01:20:19 +0900

    Split Elements from Notification Box

--- frontend/src/components/notifications/Box.jsx
+++ frontend/src/components/notifications/Box.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
-import { cubicBeizer } from "@assets/keyframes"
+import { Frame, Icon, IconSmallEmoji, ProfileImg, Texts, TextsTitle, TextsDetail, AgoAndMore, Ago } from "./Elements"
 
-import styled from "styled-components"
 import FeatherIcon from "feather-icons-react"
 import { DateTime } from "luxon"
 
@@ -100,108 +99,5 @@ const purifyNotificationForDisplay = (notification) => {
     return purified
 }
 
-const Frame = styled.article`
-display: flex;
-gap: 2em;
-
-border-bottom: 1px solid black;
-padding: 2em 2em;
-`
-
-const Icon = ({children, smallIcon}) => {
-    return <IconFrame>
-        {children}
-        {smallIcon ? <IconSmall>
-            <span>{smallIcon}</span>
-        </IconSmall> : null}
-    </IconFrame>
-}
-
-const IconFrame = styled.div`
-position: relative;
-width: auto;
-height: 3em;
-
-& svg {
-    stroke: 2em;
-    margin-right: 0;
-
-    width: auto;
-    height: 3em;
-}
-`
-
-const ProfileImg = styled.img`
-    width: 3em;
-    height: 3em;
-    border-radius: 50%;
-`
-
-const IconSmall = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-
-position: absolute;
-right: -2px;
-bottom: -2px;
-
-background-color: white;
-box-sizing: border-box;
-
-width: 1.25em;
-height: 1.25em;
-
-& span {
-    vertical-align: -0.25em;
-}
-
-& svg {
-    transform: translateX(20%);
-    width: 80%;
-    height: 80%;
-}
-`
-
-const IconSmallEmoji = styled.img`
-transform: translate(20%, 20%);
-
-width: 2.5em;
-height: auto;
-
-&:hover {
-    transform: translate(20%, 20%) scale(1.3);
-}
-
-transition: transform 0.5s ${cubicBeizer};
-`
-
-const Texts = styled.div`
-flex-grow: 999;
-display: flex;
-flex-direction: column;
-gap: 0.5em;
-justify-content: space-between;
-`
-
-const TextsTitle = styled.h3`
-font-size: 1em;
-font-weight: 600;
-`
-
-const TextsDetail = styled.p`
-font-size: 1em;
-font-weight: 400;
-`
-
-const AgoAndMore = styled.div`
-display: flex;
-gap: 0.5em;
-
-font-size: 0.75em;
-color: grey;
-`
-
-const Ago = styled.time``
 
 export default Box
