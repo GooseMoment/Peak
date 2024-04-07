7ab064480a8dab7048401cdfeab091aade13ff04
Bombwhale <solkblte@icloud.com>
2024-04-07 23:35:04 +0900

    Add useInfiniteQuery to NotificationPage

--- frontend/src/api/notifications.api.js
+++ frontend/src/api/notifications.api.js
@@ -2,10 +2,11 @@ import client from "@api/client"
 
 export const getNotifications = async (query) => {
     const params = query.queryKey[1]
+    const cursor = query.pageParam
     const types = params.types.types.join("|")
 
     try {
-        const res = await client.get(`notifications/?types=${types}`)
+        const res = await client.get(`notifications/?types=${types}&cursor=${cursor}`)
         return res.data
     } catch (e) {
         throw e
--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -1,19 +1,40 @@
-import { useState } from "react"
+import { Fragment, useState } from "react"
 
 import FilterButtonGroup from "@components/notifications/FilterButtonGroup"
 import Box from "@components/notifications/Box"
 import PageTitle from "@components/common/PageTitle"
+import Button from "@components/sign/Button"
 
 import { getNotifications } from "@api/notifications.api"
 
-import { useQuery } from "@tanstack/react-query"
+import { useInfiniteQuery } from "@tanstack/react-query"
+
+const getCursorFromURL = (url) => {
+    if (!url) return null
+    
+    const u = new URL(url)
+    const cursor = u.searchParams.get("cursor")
+    return cursor
+}
 
 const NotificationsPage = () => {
     const [activeFilter, setActiveFilter] = useState("all")
 
-    const { data, isPending, isError, error } = useQuery({ queryKey: ["notifications", {types: filters[activeFilter]}], queryFn: getNotifications })
+    const { data, isError, error, fetchNextPage, isFetching, isFetchingNextPage } = useInfiniteQuery({
+        queryKey: ["notifications", {types: filters[activeFilter]}],
+        queryFn: getNotifications,
+        initialPageParam: "",
+        getNextPageParam: (lastPage, pages) => getCursorFromURL(lastPage.next),
+    })
 
-    if (isPending) return <div>Loading</div>
+    // useInfiniteQuery에서 제공하는 hasNextPage가 제대로 작동 안함. 어째서?
+    const hasNextPage = data?.pages[data?.pages?.length-1].next !== null
+
+    const header = <>
+        <PageTitle>Notifications</PageTitle>
+        <FilterButtonGroup filters={filters} active={activeFilter} setActive={setActiveFilter} />
+    </>
+
     // TODO: add skeleton
 
     if (isError) {
@@ -22,9 +43,14 @@ const NotificationsPage = () => {
     } 
 
     return <>
-    <PageTitle>Notifications</PageTitle>
-    <FilterButtonGroup filters={filters} active={activeFilter} setActive={setActiveFilter} />
-    {data.results.map(notification => <Box key={notification.id} notification={notification} />)}
+    {header}
+    {data?.pages.map((group, i) => (
+        <Fragment key={i}>
+            {group.results.map(notification => <Box key={notification.id} notification={notification} />)}
+        </Fragment>
+    ))}
+    <Button onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>Load more</Button>
+    {isFetchingNextPage && <div>Fetching next page...</div>}
     </>
 }
 
