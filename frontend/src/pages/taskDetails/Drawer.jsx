de1e00b50ebb5451ef4d6b1e4b18651526ed9267
minyoy <minyoy@naver.com>
2024-04-09 23:22:47 +0900

    Create DrawerFolder at TaskDetail - Drawer

--- frontend/src/pages/taskDetails/Drawer.jsx
+++ frontend/src/pages/taskDetails/Drawer.jsx
@@ -1,18 +1,14 @@
 import { useRouteLoaderData, useNavigate, useOutletContext, useSubmit } from "react-router-dom"
-import { useState, Fragment } from "react"
-
-import styled from "styled-components"
-import FeatherIcon from "feather-icons-react"
+import { Fragment } from "react"
 
 import Detail from "@components/project/common/Detail"
+import DrawerFolder from "@components/project/Creates/DrawerFolder"
 
 const Drawer = ({ projectId, task }) => {
     const { projects } = useRouteLoaderData("app")
     const navigate = useNavigate()
     const submit = useSubmit()
     const [closeComponent] = useOutletContext()
-
-    const [collapsed, setCollapsed] = useState(false)
 
     const changeDrawer = (drawerId) => {
         return async () => {
@@ -28,59 +24,11 @@ const Drawer = ({ projectId, task }) => {
         <Detail title="서랍 선택" onClose={closeComponent}>
             {projects.map((project) => (
                 <Fragment key={project.id}>
-                    <ItemBox onClick={() => setCollapsed(prev => !prev)}>
-                        <Circle $color={project.color}/>
-                        <ItemText $is_project={true}>{project.name}</ItemText>
-                    </ItemBox>
-                    {collapsed ? null : 
-                        project.drawers && project.drawers.map(drawer => (
-                        <ItemBox key={drawer.id} onClick={changeDrawer(drawer.id)}>
-                            <FeatherIcon icon="arrow-right"/>
-                            <ItemText $is_project={false}>{drawer.name}</ItemText>
-                        </ItemBox>
-                    ))}
+                    <DrawerFolder project={project} changeDrawer={changeDrawer}/>
                 </Fragment>
             ))}
         </Detail>
     )
 }
-
-const Circle = styled.div`
-    position: relative;
-    width: 1.1em;
-    height: 1.1em;
-    background-color: #${props => props.$color};
-    border-radius: 50%;
-    margin-right: 0.6em;
-`
-
-const ItemBox = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    margin-left: 1.2em;
-    margin-top: 1.2em;
-
-    & svg {
-        margin-left: 1.3em;
-        top: 0;
-    }
-`
-
-const ItemText = styled.div`
-    width: 10em;
-    font-weight: ${props => props.$is_project ? '500' : 'normal'};
-    font-size: 1em;
-    color: #000000;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-
-    &:hover {
-        font-weight: bolder;
-        color: #FF4A03;
-        cursor: pointer;
-    }
-`
 
 export default Drawer
