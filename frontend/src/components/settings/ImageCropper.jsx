038ff8dd6a21057abf001c9efed26a5afe54f54c
Bombwhale <solkblte@icloud.com>
2024-04-12 02:23:02 +0900

    Edit StyledButtonGroup in ImageCropper

--- frontend/src/components/settings/ImageCropper.jsx
+++ frontend/src/components/settings/ImageCropper.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 
-import Button, { buttonForms } from "@components/common/Button"
+import Button, { buttonForms, ButtonGroup } from "@components/common/Button"
 
 import Cropper from "react-easy-crop"
 import styled from "styled-components"
-import { states } from "@/assets/themes"
+import { states } from "@assets/themes"
 
-const ImageCropper = ({file, setCroppedAreaPixels, onClickOk}) => {
+const ImageCropper = ({file, setCroppedAreaPixels, onClickOk, onClickCancel}) => {
     const [crop, setCrop] = useState({ x: 0, y: 0 })
     const [zoom, setZoom] = useState(1)
 
@@ -24,22 +24,25 @@ const ImageCropper = ({file, setCroppedAreaPixels, onClickOk}) => {
         onCropComplete={onCropComplete}
         onZoomChange={setZoom}
     />
-    <ButtonGroup>
-        <Button form={buttonForms.OUTLINED} state={states.DANGER}>Cancel</Button>
-        <Button form={buttonForms.FILLED} state={states.SUCCESS} onClick={onClickOk}>Apply</Button>
-    </ButtonGroup>
+    <StyledButtonGroup>
+        <Button $form={buttonForms.OUTLINED} $state={states.DANGER} onClick={onClickCancel}>Cancel</Button>
+        <Button $form={buttonForms.FILLED} $state={states.SUCCESS} onClick={onClickOk}>Apply</Button>
+    </StyledButtonGroup>
     </>
 }
 
-const ButtonGroup = styled.div`
+const StyledButtonGroup = styled(ButtonGroup)`
     position: fixed;
     z-index: 1000;
-    left: auto;
-    right: auto;
-    bottom: 6rem;
+    left: 50%;
+    top: 70%;
+    transform: translateX(-50%) translateY(200%);
 
-    display: flex;
-    gap: 2rem;
+    padding: 1em;
+    border-radius: 10px;
+    background-color: ${p => p.theme.backgroundColor};
+
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 `
 
 export default ImageCropper
