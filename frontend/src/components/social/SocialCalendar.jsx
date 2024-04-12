62feb478c3d6fbb0cd26bfa388736431347e7113
andless <andless2004@gmail.com>
2024-04-12 16:35:36 +0900

    Remove unused import

--- frontend/src/components/social/SocialCalendar.jsx
+++ frontend/src/components/social/SocialCalendar.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components"
 import CommonCalendar from "@components/common/CommonCalendar";
-import moment from 'moment';
 
 import { useState } from "react";
 
@@ -14,16 +13,10 @@ const SocialCalendar = ({newLogDates, selectedDate, setSelectedDate}) => {
       selectedEndDate={tempDate}
       setSelectedEndDate={setTempDate}
       contentedDates={newLogDates}
-      // tileClassName={({ date, view }) => {
-      //   if (newLogDates.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
-      //     return "highlight";
-      //   }
-      // }}
     />
   </>
 };
 
-// TODO: divide each calendar style
 const StyledSocialCalendar = styled(CommonCalendar)``
 
 export default SocialCalendar;
