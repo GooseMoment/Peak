d28eb376e00c3ac35468e85af3a30057ce8911cf
minyoy <minyoy@naver.com>
2024-04-16 10:59:15 +0900

    Add QueryClientPriovider at AppLayout

--- frontend/src/containers/AppLayout.jsx
+++ frontend/src/containers/AppLayout.jsx
@@ -1,14 +1,18 @@
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 
 import AuthGuard from "@components/auth/AuthGuard"
 import Layout from "@containers/Layout"
 
+import { QueryClientProvider } from "@tanstack/react-query"
+import queryClient from "@queries/queryClient"
+
 const AppLayout = () => {
-    const location = useLocation()
     return <AuthGuard>
-        <Layout>
-            <Outlet />
-        </Layout>
+        <QueryClientProvider client={queryClient}>
+            <Layout>
+                <Outlet />
+            </Layout>
+        </QueryClientProvider>
     </AuthGuard>
 }
 
