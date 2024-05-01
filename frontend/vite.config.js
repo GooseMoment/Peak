b17f6ca2c625ac4195bbbad4bad33e7838a2a530
Bombwhale <solkblte@icloud.com>
2024-05-01 22:32:50 +0900

    Remove vite-plugin-pwa Add app.webmanifest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import path from "path"
 
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,75 +12,6 @@ export default defineConfig({
     },
     plugins: [
         react(),
-        VitePWA({
-            registerType: "autoUpdate",
-            injectRegister: "script-defer",
-            workbox: {
-                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-            },
-            devOptions: {
-                enabled: true,
-            },
-            manifest: {
-                name: 'Peak',
-                short_name: 'Peak',
-                description: 'Pick, Peek, Peck, and Peak.',
-                theme_color: '#ff4a03',
-                background_color: '#ffffff',
-                icons: [
-                    {
-                        "src": "logo.svg",
-                        "sizes": "any",
-                        "type": "image/svg+xml",
-                        "purpose": "maskable",
-                    },
-                    {
-                        "src": "pwa-64x64.png",
-                        "sizes": "64x64",
-                        "type": "image/png"
-                    },
-                    {
-                        "src": "pwa-192x192.png",
-                        "sizes": "192x192",
-                        "type": "image/png"
-                    },
-                    {
-                        "src": "pwa-512x512.png",
-                        "sizes": "512x512",
-                        "type": "image/png",
-                        "purpose": "maskable",
-                    },
-                    {
-                        "src": "maskable-icon-512x512.png",
-                        "sizes": "512x512",
-                        "type": "image/png",
-                        "purpose": "maskable"
-                    }
-                ],
-                shortcuts: [
-                    {
-                        "name": "Notifications",
-                        "url": "http://localhost:8080/app/notifications",
-                        "short_name": "Noti",
-                    },
-                    {
-                        "name": "Today",
-                        "url": "http://localhost:8080/app/today",
-                        "short_name": "Today",
-                    },
-                    {
-                        "name": "Social",
-                        "url": "http://localhost:8080/app/social/following",
-                        "short_name": "Social",
-                    },
-                    {
-                        "name": "Projects",
-                        "url": "http://localhost:8080/app/projects/",
-                        "short_name": "Projects",
-                    },
-                ],
-            },
-        }),
     ],
     resolve: {
         alias: [
