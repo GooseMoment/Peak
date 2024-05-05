d4db43fe06154116041d6b173d37e177d3b957ed
minyoy <minyoy@naver.com>
2024-05-05 21:17:43 +0900

    Add Task Pagination

--- frontend/src/components/project/Drawer.jsx
+++ frontend/src/components/project/Drawer.jsx
@@ -1,28 +1,43 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-import styled from "styled-components"
+import styled, { keyframes, css } from "styled-components"
+import { cubicBeizer } from "@assets/keyframes"
 import FeatherIcon from 'feather-icons-react'
 
 import Task from "@components/project/Task"
 import TaskCreateSimple from "@components/project/Creates/TaskCreateSimple"
 
-import { useQuery } from "@tanstack/react-query"
+import { useInfiniteQuery } from "@tanstack/react-query"
 import { getTasksByDrawer } from "@api/tasks.api"
+import Button from "../sign/Button"
 
-function Drawer({project, drawer, color}){
+const getPageFromURL = (url) => {
+    if (!url) return null
+    
+    const u = new URL(url)
+    const page = u.searchParams.get("page")
+    return page
+}
+
+const Drawer = ({project, drawer, color}) => {
     const navigate = useNavigate()
 
-    const { isPending, isError, data: tasks, error } = useQuery({
-        queryKey: ['tasks', {drawerID: drawer.id}],
-        queryFn: () => getTasksByDrawer(drawer.id),
+    const { data, isError, error, fetchNextPage, isFetching, isFetchingNextPage } = useInfiniteQuery({
+        queryKey: ["tasks", {drawerID: drawer.id}],
+        queryFn: (pages) => getTasksByDrawer(drawer.id, pages.pageParam || 1),
+        initialPageParam: 1,
+        getNextPageParam: (lastPage) => getPageFromURL(lastPage.next),
     })
+
+    // useInfiniteQuery에서 제공하는 hasNextPage가 제대로 작동 안함. 어째서?
+    const hasNextPage = data?.pages[data?.pages?.length-1].next !== null
 
     //Drawer collapsed handle
     const [collapsed, setCollapsed] = useState(false);
     
     const handleCollapsed = () => {
-        { drawer.task_count !== 0 && setCollapsed(prev => !prev)}
+        {drawer.task_count !== 0 && setCollapsed(prev => !prev)}
     }
 
     //simpleCreateTask handle
@@ -38,9 +53,11 @@ function Drawer({project, drawer, color}){
     }
 
     const drawerIcons = [
-        {icon: "plus", click: clickPlus},
-        {icon: "chevron-down", click: handleCollapsed},
-        {icon: "more-horizontal", click: () => {}},
+        {icon: <FeatherIcon icon={"plus"} onClick={clickPlus}/>},
+        {icon: <CollapseButton $collapsed={collapsed}>
+            <FeatherIcon icon={"chevron-down"} onClick={handleCollapsed}/>
+        </CollapseButton>},
+        {icon: <FeatherIcon icon={"more-horizontal"}/>},
     ]
 
     return (
@@ -49,24 +66,28 @@ function Drawer({project, drawer, color}){
                 <DrawerName $color = {color}>{drawer.name}</DrawerName>
                 <DrawerIcon $color = {color}>
                     {drawerIcons.map(item => (
-                        <FeatherIcon key={item.icon} icon={item.icon} onClick={item.click}/>
+                        item.icon
                     ))}
                 </DrawerIcon>
             </DrawerBox>
             {collapsed ? null :
                 <TaskList>
-                    {tasks && tasks.map((task) => (
-                        <Task key={task.id} projectId={project.id} task={task} color={color}/>
-                    ))}
+                    {data?.pages.map((group, i) => (
+                        group.results.map((task) =>
+                            <Task key={task.id} projectId={project.id} task={task} color={color}/>
+                    )))}
                 </TaskList>
             }
-            { isSimpleOpen &&
+            {isSimpleOpen &&
                 <TaskCreateSimple color={color}/>
             }
             <TaskCreateButton onClick={handleisSimpleOpen}>
                 <FeatherIcon icon="plus-circle"/>
                 <TaskCreateText>할 일 추가</TaskCreateText>
             </TaskCreateButton>
+            <FlexBox>
+                {hasNextPage ? <Button onClick={() => fetchNextPage()}>더보기</Button> : null}
+            </FlexBox>
         </>
     );
 }
@@ -108,6 +129,38 @@ const DrawerIcon = styled.div`
     }
 `
 
+const rotateToUp = keyframes`
+    0% {
+        transform: rotate(-180deg);
+    }
+
+    100% {
+        transform: rotate(0deg);
+    }
+`
+
+const rotateToUnder = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(-180deg);
+    }
+`
+
+const CollapseButton = styled.div`
+    & svg {
+        animation: ${rotateToUp} 0.5s ${cubicBeizer} forwards;
+    }
+
+    ${props => props.$collapsed && css`
+        & svg {
+            animation: ${rotateToUnder} 0.5s ${cubicBeizer} forwards;
+        }
+    `}
+`
+
 const TaskList = styled.div`
     flex: 1;
     margin-left: 0.5em;
@@ -135,8 +188,15 @@ const TaskCreateButton = styled.div`
 const TaskCreateText = styled.div`
     font-size: 1.1em;
     font-weight: medium;
-    color: #000000;
+    color: black;
     margin-top: 0em;
+`
+
+const FlexBox = styled.div`
+    display: flex;
+    margin-top: 1em;
+    align-items: center;
+    justify-content: center;
 `
 
 export default Drawer
