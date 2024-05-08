9b089f4bc81366a07fd90e478d2974dc70f54c72
Bombwhale <solkblte@icloud.com>
2024-05-08 19:31:37 +0900

    Add signal receivers for Following and Comment

--- backend/notifications/signals.py
+++ backend/notifications/signals.py
@@ -2,8 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Notification
-# TODO: Add Following here after Following.status added
-from social.models import Reaction, Peck
+from social.models import Reaction, Peck, Following, Comment
 from .push import pushNotificationToUser
 
 # TODO: add code for TaskReminder
@@ -34,6 +33,36 @@ def create_notification_for_peck(sender, instance: Peck=None, created=False, **k
 
     return Notification.objects.create(
         user=target_user, peck=instance, type=noti_type,
+    )
+
+@receiver(post_save, sender=Following)
+def create_notification_for_following(sender, instance: Following=None, created=False, **kwargs):
+    match instance.status:
+        case Following.REQUESTED:
+            return Notification.objects.create(
+                user=instance.followee, type=Notification.FOR_FOLLOW_REQUEST, following=instance, created_at=instance.created_at,
+            )
+        case Following.ACCEPTED:
+            noti1 = Notification.objects.create(
+                user=instance.follower, type=Notification.FOR_FOLLOW_REQUEST_ACCEPTED, following=instance, created_at=instance.created_at,
+            )
+
+            noti2 = Notification.objects.create(
+                user=instance.followee, type=Notification.FOR_FOLLOW, following=instance, created_at=instance.updated_at,
+            )
+
+            return (noti1, noti2, )
+        case Following.REJECTED, Following.CANCELED:
+            # no notification for these types
+            pass
+
+@receiver(post_save, sender=Comment)
+def create_notification_for_comment(sender, instance: Comment=None, created=False, **kwargs):
+    if not created:
+        return
+
+    return Notification.objects.create(
+        user=instance.task.user, type=Notification.FOR_COMMENT, comment=instance, created_at=instance.created_at,
     )
 
 @receiver(post_save, sender=Notification)
