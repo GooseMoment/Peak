ea8a268be17a4c40cb3a906d9d4c59f9e62d1b00
andless <andless2004@gmail.com>
2024-05-11 19:32:01 +0900

    Add django-redis with simple healthcheck

--- backend/django_peak/settings.py
+++ backend/django_peak/settings.py
@@ -203,3 +203,12 @@ STORAGES = {
     },
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis-cache:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
