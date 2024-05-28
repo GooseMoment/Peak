b06dda86c7c82d20e520584203f6aa42c67bba06
minyoy <minyoy@naver.com>
2024-05-28 17:09:48 +0900

    Create (un)completed_task_count signals

---
+++ backend/drawers/signals.py
@@ -0,0 +1,31 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from tasks.models import Task
+
+@receiver(post_save, sender=Task)
+def add_task_count_for_Task(sender, instance: Task=None, created=False, **kwargs):
+    if not created:
+        return
+
+    if instance.completed_at is None:
+        instance.drawer.uncompleted_task_count += 1
+        print(instance.drawer.uncompleted_task_count)
+    else:
+        instance.drawer.completed_task_count += 1
+        print(instance.drawer.completed_task_count)
+
+    instance.drawer.save()
+
+@receiver(post_delete, sender=Task)
+def delete_task_count_for_Task(sender, instance: Task=None, **kwargs):
+    if instance.completed_at is None:
+        instance.drawer.uncompleted_task_count -= 1
+        print(instance.drawer.uncompleted_task_count)
+    else:
+        instance.drawer.completed_task_count -= 1
+        print(instance.drawer.completed_task_count)
+
+    instance.drawer.save()
+
+        
