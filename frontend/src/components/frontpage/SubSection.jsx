edead63bf6874fb0a8bd05911d5ba346e4419e8e
Bombwhale <solkblte@icloud.com>
2024-06-28 17:15:10 +0900

    Add Section and SubSection for FrontPage

---
+++ frontend/src/components/frontpage/SubSection.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const SubSection = styled.article`
+`
+export default SubSection
+
+export const SubGroup = styled.div`
+    width: 100%;
+    margin-top: 3em;
+
+    display: flex;
+    gap: 2em;
+
+    flex-wrap: wrap;
+    align-items: stretch;
+    justify-content: space-evenly;
+`
+
+export const SubTitle = styled.h3`
+    font-weight: 600;
+    margin-bottom: 2em;
+`
