98a18e281dc26e84dab4ca406e48e6e8ef9f4b0f
Bombwhale <solkblte@icloud.com>
2024-07-05 15:06:55 +0900

    Add button for DemoProject

--- frontend/src/components/frontpage/DemoProject.jsx
+++ frontend/src/components/frontpage/DemoProject.jsx
@@ -1,16 +1,27 @@
+import { useMemo } from "react"
+
 import SubSection, { SubTitle } from "@components/frontpage/SubSection"
 import ProjectName from "@components/project/ProjectName"
-import { useMemo } from "react"
+import Button, { ButtonGroup } from "@components/common/Button"
+
 import { useTranslation } from "react-i18next"
+import { useState } from "react"
 
 const DemoProject = () => {
     const {t} = useTranslation(null, {keyPrefix: "frontpage.section_organize.Sub_project"}) 
+    const [count, setCount] = useState(1)
     const projects = useMemo(() => makeProjects(t), [t])
 
     return <SubSection>
         <SubTitle>A project contains drawers.</SubTitle>
 
-        {projects?.map(project => <ProjectName key={project.id} project={project} demo />)}
+        {projects?.slice(0, count)?.map(project => <ProjectName key={project.id} project={project} demo />)}
+
+        {count < projects.length && 
+            <ButtonGroup $justifyContent="right" $margin="1em 0">
+                <Button onClick={() => setCount(count + 1)}>Add project</Button>
+            </ButtonGroup>
+        }
     </SubSection>
 }
 
