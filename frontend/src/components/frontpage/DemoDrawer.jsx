afa72c45348b5ce4c7e78115ea70342ac39a8f1a
Bombwhale <solkblte@icloud.com>
2024-07-05 15:07:33 +0900

    Add sample tasks in DemoDrawer

--- frontend/src/components/frontpage/DemoDrawer.jsx
+++ frontend/src/components/frontpage/DemoDrawer.jsx
@@ -1,10 +1,10 @@
 import { useMemo } from "react"
 
+import PageTitle from "@components/common/PageTitle"
 import SubSection, { SubTitle } from "@components/frontpage/SubSection"
 import DrawerBox, { DrawerName } from "@components/project/DrawerBox"
 
 import { useTranslation } from "react-i18next"
-import PageTitle from "../common/PageTitle"
 
 const DemoDrawer = () => {
     const {t} = useTranslation(null, {keyPrefix: "frontpage.section_organize.Sub_drawer"}) 
@@ -14,32 +14,69 @@ const DemoDrawer = () => {
         <SubTitle>A drawer is home for tasks.</SubTitle>
 
         <PageTitle $color="#FD99E1">한양라이프</PageTitle>
-        {drawers?.map(drawer => <DrawerBox key={drawer.color} $color={drawer.color} $demo>
-            <DrawerName $color={drawer.color} $demo>{drawer.name}</DrawerName>
+        {drawers?.map((drawer, i) => <DrawerBox key={i} $color="FD99E1" $demo>
+            <DrawerName $color="FD99E1" $demo>{drawer.name}</DrawerName>
         </DrawerBox>)}
     </SubSection>
 }
 
 const makeDrawers = (t) => [
     {
-        name: t("drawers.drawer1"),
-        color: "FD99E1",
+        name: t("drawers.drawer1.name"),
+        tasks: [
+            {
+                name: t("drawers.drawer1.tasks.task1"),
+                completed_at: new Date(),
+                due_date: null,
+                due_time: null,
+                assigned_at: null,
+                priority: null,
+            },
+            {
+                name: t("drawers.drawer1.tasks.task2"),
+                completed_at: new Date(),
+                due_date: null,
+                due_time: null,
+                assigned_at: null,
+                priority: null,
+            },
+            {
+                name: t("drawers.drawer1.tasks.task3"),
+                completed_at: new Date(),
+                due_date: null,
+                due_time: null,
+                assigned_at: null,
+                priority: null,
+            },
+            {
+                name: t("drawers.drawer1.tasks.task4"),
+                completed_at: new Date(),
+                due_date: null,
+                due_time: null,
+                assigned_at: null,
+                priority: null,
+            },
+            {
+                name: t("drawers.drawer1.tasks.task5"),
+                completed_at: new Date(),
+                due_date: null,
+                due_time: null,
+                assigned_at: null,
+                priority: null,
+            },
+        ]
     },
     {
         name: t("drawers.drawer2"),
-        color: "#",
     },
     {
         name: t("drawers.drawer3"),
-        color: "#",
     },
     {
         name: t("drawers.drawer4"),
-        color: "#",
     },
     {
         name: t("drawers.drawer5"),
-        color: "#",
     },
 ]
 
