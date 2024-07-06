0d127250087f1667e398c36bb7b2b24999b24f69
Bombwhale <solkblte@icloud.com>
2024-07-06 17:52:05 +0900

    Add icons and sampleTasks in DemoPlan

--- frontend/src/components/intro/DemoPlan.jsx
+++ frontend/src/components/intro/DemoPlan.jsx
@@ -1,12 +1,20 @@
 import SubSection from "@components/intro/SubSection"
 import RadioGroup from "@components/intro/RadioGroup"
 import Radio from "@components/intro/Radio"
-import Task from "@components/tasks/Task"
+import DemoTask from "@components/intro/DemoTask"
 
-import {today, tomorrow, nextWeek, twoWeeksLater} from "./todays"
+import {yesterday, today, tomorrow, nextWeek, twoWeeksLater} from "./todays"
 
 import styled from "styled-components"
 import { useState } from "react"
+
+import normal from "@assets/project/priority/normal.svg"
+import important from "@assets/project/priority/important.svg"
+import critical from "@assets/project/priority/critical.svg"
+
+import todayIcon from "@assets/project/calendar/today.svg"
+import tomorrowIcon from "@assets/project/calendar/tomorrow.svg"
+import next_weekIcon from "@assets/project/calendar/next_week.svg"
 
 const deadlineTable = {
     today,
@@ -14,6 +22,13 @@ const deadlineTable = {
     nextWeek,
     twoWeeksLater,
 }
+
+const sampleTasks = [
+    {id: 0, name: "토플 공부하기", completed_at: true, priority: 0, due_date: today},
+    {id: 1, name: "건강검진 받기", completed_at: true, priority: 1, due_date: yesterday},
+    {id: 3, name: "썬크림 사기", completed_at: true, priority: 2, due_date: tomorrow},
+    {id: 4, name: "퇴사하기", completed_at: true, priority: 0, due_date: nextWeek},
+]
 
 const DemoPlan = () => {
     const [priority, setPriority] = useState("1")
@@ -29,23 +44,29 @@ const DemoPlan = () => {
         <HalfDivider>
             <Selections>
                 <Selection>
-                    <RadioGroup label="Choose a priority" value={priority} onChange={value => setPriority(value)}>
-                        <Radio value="0">Normal</Radio>
-                        <Radio value="1">Important</Radio>
-                        <Radio value="2">Critical</Radio>
+                    <RadioGroup label="Choose a priority" value={priority} onChange={setPriority}>
+                        <Radio value="0"><Icon src={normal} /> Normal</Radio>
+                        <Radio value="1"><Icon src={important} /> Important</Radio>
+                        <Radio value="2"><Icon src={critical} /> Critical</Radio>
                     </RadioGroup>
                 </Selection>
                 <Selection>
-                    <RadioGroup label="Choose a deadline" value={deadline} onChange={value => setDeadline(value)}>
-                        <Radio value="today">Today</Radio>
-                        <Radio value="tomorrow">Tomorrow</Radio>
-                        <Radio value="nextWeek">Next week</Radio>
-                        <Radio value="twoWeeksLater">Two weeks later</Radio>
+                    <RadioGroup label="Choose a deadline" value={deadline} onChange={setDeadline}>
+                        <Radio value="today"><Icon src={todayIcon} /> Today</Radio>
+                        <Radio value="tomorrow"><Icon src={tomorrowIcon} /> Tomorrow</Radio>
+                        <Radio value="nextWeek"><Icon src={next_weekIcon} /> Next week</Radio>
+                        <Radio value="twoWeeksLater"><Icon src={next_weekIcon} /> Two weeks later</Radio>
                     </RadioGroup>
                 </Selection>
             </Selections>
             <Tasks>
-                <Task task={task} demo />
+                <BlurArea>
+                    {sampleTasks.slice(0, 1).map(task => <DemoTask task={task} key={task.id} />)}
+                </BlurArea>
+                <DemoTask id={2} task={task} />
+                <BlurArea>
+                    {sampleTasks.slice(1).map(task => <DemoTask task={task} key={task.id} />)}
+                </BlurArea>
             </Tasks>
         </HalfDivider>
     </SubSection>
@@ -67,8 +88,18 @@ const Selection = styled.div`
 
 `
 
+const Icon = styled.img`
+    aspect-ratio: 1/1;
+    height: 1em;
+    vertical-align: bottom;
+`
+
 const Tasks = styled.div`
     width: 50%;
+`
+
+const BlurArea = styled.div`
+    filter: blur(1px);
 `
 
 export default DemoPlan
--- frontend/src/components/intro/Radio.jsx
+++ frontend/src/components/intro/Radio.jsx
@@ -6,15 +6,17 @@ import styled from "styled-components"
 
 const Radio = ({ children, value, name, defaultChecked, disabled }) => {
     const group = useContext(RadioContext)
+    const checked = group.value !== undefined ? value === group.value : undefined
+
     return (
-        <Label>
-            <input
+        <Label $checked={checked}>
+            <RadioInput
                 type="radio"
                 value={value}
                 name={name}
                 defaultChecked={defaultChecked}
                 disabled={disabled || group.disabled}
-                checked={group.value !== undefined ? value === group.value : undefined}
+                checked={checked}
                 onChange={(e) => group.onChange && group.onChange(e.target.value)}
             />
             {children}
@@ -25,6 +27,15 @@ const Radio = ({ children, value, name, defaultChecked, disabled }) => {
 const Label = styled.label`
     display: block;
     margin-top: 1em;
+
+    color: ${p => p.$checked ? p.theme.accentColor : p.theme.textColor};
+    font-weight: ${p => p.$checked ? 800 : 400};
+
+    cursor: pointer;
+`
+
+const RadioInput = styled.input`
+    visibility: hidden;
 `
 
 export default Radio
