fff4789d5f916db5dea2b13d386be6b8d25a54b3
Bombwhale <solkblte@icloud.com>
2024-07-06 16:00:24 +0900

    FIx Priority margin-top

--- frontend/src/components/tasks/Priority.jsx
+++ frontend/src/components/tasks/Priority.jsx
@@ -1,30 +1,26 @@
-import styled, { css } from "styled-components"
-
 import priority2 from "@assets/project/priority/priority2.svg"
 import priority1 from "@assets/project/priority/priority1.svg"
 import priority0 from "@assets/project/priority/priority0.svg"
 
-const Priority = ({priority, completed}) => {
+import styled, { css } from "styled-components"
 
-    
+const Priority = ({priority, completed, hasDate}) => {
     const prioritys = [
-        <PriorityImg $completed={completed} src={priority0}/>,
-        <PriorityImg $completed={completed} src={priority1}/>,
-        <PriorityImg $completed={completed} src={priority2}/>,
+        <PriorityImg $completed={completed} $hasDate={hasDate} src={priority0}/>,
+        <PriorityImg $completed={completed} $hasDate={hasDate} src={priority1}/>,
+        <PriorityImg $completed={completed} $hasDate={hasDate} src={priority2}/>,
     ]
 
-    return (
-        prioritys[priority]
-    )
+    return prioritys[priority]
 }
 
 const PriorityImg = styled.img`
     width: 15px;
     height: 15px;
-    margin-top: 0.2em;
+    margin-top: ${p => p.$hasDate ? "-0.5em" : "0em"};
     margin-right: 0.2em;
     filter: ${(props) => (props.$completed && css`
-    invert(73%) sepia(3%) saturate(9%) hue-rotate(349deg) brightness(89%) contrast(92%);
+        invert(73%) sepia(3%) saturate(9%) hue-rotate(349deg) brightness(89%) contrast(92%);
     `)};
 `
 
