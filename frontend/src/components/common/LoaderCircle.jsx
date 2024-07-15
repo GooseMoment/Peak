ee6edbde3044ba8ad23f10c444902d3225102fed
minyoy <minyoy@naver.com>
2024-07-15 14:59:12 +0900

    Add Reminder etc

---
+++ frontend/src/components/common/LoaderCircle.jsx
@@ -0,0 +1,30 @@
+import styled, { keyframes } from "styled-components"
+
+const LoaderCircle = () => {
+    return (
+        <Loader/>
+    )
+}
+
+const Spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const Loader = styled.div`
+  width: 0.5em;
+  height: 0.5em;
+  border: 2.5px solid rgba(0, 0, 0, 0.25);
+  border-left-color: transparent;
+  border-radius: 50%;
+
+  animation: ${Spin} 1s linear infinite;
+
+`
+
+export default LoaderCircle
