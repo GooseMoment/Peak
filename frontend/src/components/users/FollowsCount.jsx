03267ef5c66feba36a737425038d5da1274a881b
Bombwhale <solkblte@icloud.com>
2024-07-15 17:06:45 +0900

    Edit FollowsCount imports

--- frontend/src/components/users/FollowsCount.jsx
+++ frontend/src/components/users/FollowsCount.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import ModalPortal from "@components/common/ModalPortal"
-import { FollowerList, FollowingList } from "./FollowList"
+import { FollowerList, FollowingList } from "@components/users/FollowList"
 
 import styled from "styled-components"
-import { useEffect } from "react"
 
 const FollowsCount = ({user}) => {
     const [window, setWindow] = useState("")
