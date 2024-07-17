66b5bcd26817938028fac4397849c9ae0fd8bc3f
minyoy <65756020+minyoy@users.noreply.github.com>
2024-07-17 17:11:03 +0900

    Issue 해결 : TaskCreate 생성오류 및 ProjectListPage 문자잘림 (#135)
    * Fix CuttingProblem at ProjectName
    
    * Add TaskCreate ErrorMessage
    
    * Add due_tz null and blank True
    
    * Fix conditional syntax / Fix #132 #134

--- frontend/src/components/project/taskDetails/TaskCreate.jsx
+++ frontend/src/components/project/taskDetails/TaskCreate.jsx
@@ -49,7 +49,10 @@ const TaskCreate = () => {
             queryClient.invalidateQueries({queryKey: ['tasks', {drawerID: state?.drawer_id}]})
             onClose()
         } catch (e) {
-            toast.error("할 일 생성에 실패하였습니다.")
+            if (newTask?.name)
+                toast.error("할 일 생성에 실패하였습니다.")
+            else
+                toast.error("할 일의 이름을 적어주세요.")
         }
     }
 
