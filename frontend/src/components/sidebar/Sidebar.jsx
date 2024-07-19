af879ff4e831a93c59768f321d68e22378dce377
Bombwhale <solkblte@icloud.com>
2024-07-20 01:20:20 +0900

    Remove RouteLoadData in Sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,51 +1,55 @@
-import { useRouteLoaderData } from "react-router-dom"
-
 import Header from "./Header"
 import Middle from "./Middle"
 import Footer from "./Footer"
 
+import { getProjectList } from "@api/projects.api"
+import { getMe } from "@api/users.api"
+
 import styled, { css } from "styled-components"
+import { useQuery } from "@tanstack/react-query"
 
 const Sidebar = ({collapsed, setCollapsed}) => {
-    const routeData = useRouteLoaderData("app")
+    const { data: user, isPending: userPending, isError: userError } = useQuery({
+        queryKey: ["users", "me"],
+        queryFn: () => getMe(),
+    })
 
-    if (!routeData) {
-        return null
-    }
-
-    const {projects, user} = routeData
+    const { data: projects, isPending: projectPending, isError: projectError } = useQuery({
+        queryKey: ["projects"],
+        queryFn: () => getProjectList(),
+    })
 
     return <SidebarBox $collapsed={collapsed}>
         <Header collapsed={collapsed} setCollapsed={setCollapsed} />
-        <Middle collapsed={collapsed} projects={projects} />
-        <Footer collapsed={collapsed} user={user} />
+        <Middle collapsed={collapsed} projects={projects} isPending={projectPending} isError={projectError} />
+        <Footer collapsed={collapsed} user={user} isPending={userPending} isError={userError} />
     </SidebarBox>
 }
 
 export const SidebarBox = styled.nav`
-z-index: 99;
+    z-index: 99;
 
-position: fixed;
-height: 100vh;
-width: 18rem;
+    position: fixed;
+    height: 100vh;
+    width: 18rem;
 
-display: flex;
-flex-direction: column;
-justify-content: space-between;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
-color: ${p => p.theme.textColor};
-background-color: ${p => p.theme.sidebar.backgroundColor};
+    color: ${p => p.theme.textColor};
+    background-color: ${p => p.theme.sidebar.backgroundColor};
 
-${({$collapsed}) => $collapsed ? css`
-    width: unset;
-` : null}
+    & * {
+        user-select: none;
+        -ms-user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+    }
 
-& * {
-    user-select: none;
-    -ms-user-select: none;
-    -moz-user-select: none;
-    -webkit-user-select: none;
-}
+    ${({$collapsed}) => $collapsed && css`
+        width: unset;
+    `}
 `
 
 export default Sidebar
