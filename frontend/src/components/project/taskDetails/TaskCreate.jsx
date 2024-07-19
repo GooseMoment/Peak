f3f3c5e43bd5df19c58b0b0fff77638291652325
minyoy <65756020+minyoy@users.noreply.github.com>
2024-07-19 13:09:31 +0900

    Project, Drawer, Task의 Delete 기능 추가 (#143)
    * Create TaskDelete
    
    * Create ContextMenu frontend
    
    * Edit ProjectList, Project loader->query
    
    * Create DrawerDelete function
    
    * Add delete_task_after_alert clientSetting
    
    * Create DeleteAlert
    
    * Remove trash icon at TaskCreate
    
    * Fix ProjectListPage Typo Error
    
    * Fix #145
    
    * Rename handleIsContextMenu -> handleToggleContextMenu
    
    * Remover css-hover
    
    * Add delete success toast

--- frontend/src/components/project/taskDetails/TaskCreate.jsx
+++ frontend/src/components/project/taskDetails/TaskCreate.jsx
@@ -67,7 +67,6 @@ const TaskCreate = () => {
                     color={color} 
                 />
                 <Icons>
-                    <FeatherIcon icon="trash-2" />
                     <FeatherIcon icon="x" onClick={onClose} />
                 </Icons>
             </TaskNameBox>
--- frontend/src/pages/ProjectPage.jsx
+++ frontend/src/pages/ProjectPage.jsx
@@ -1,23 +1,81 @@
-import { Outlet, useLoaderData, useParams } from "react-router-dom"
-import { useState } from "react"
+import { useNavigate, Outlet, useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
 
-import styled from "styled-components"
+import styled, { useTheme } from "styled-components"
 import FeatherIcon from "feather-icons-react"
 
 import PageTitle from "@components/common/PageTitle"
 import Drawer from "@components/drawers/Drawer"
 import DrawerCreate from "@components/project/Creates/DrawerCreate"
+import ContextMenu from "@components/common/ContextMenu"
+import DeleteAlert from "@components/common/DeleteAlert"
 import ModalPortal from "@components/common/ModalPortal"
+import queryClient from "@queries/queryClient"
+import handleToggleContextMenu from "@utils/handleToggleContextMenu"
+
+import { toast } from "react-toastify"
 import { useTranslation } from "react-i18next"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { getProject, patchProject, deleteProject } from "@api/projects.api"
 
 const ProjectPage = () => {
     const { id } = useParams()
-    const { project } = useLoaderData()
-    const drawers = project.drawers
-
-    const { t } = useTranslation(null, {keyPrefix: "project"})
+    const theme = useTheme()
+    const navigate = useNavigate()
 
     const [isDrawerCreateOpen, setIsDrawerCreateOpen] = useState(false)
+    const [isContextMenuOpen, setIsContextMenuOpen] = useState(false)
+    const [isAlertOpen, setIsAlertOpen] = useState(false)
+    const [selectedButtonPosition, setSelectedButtonPosition] = useState({top: 0, left: 0})
+
+    const { t } = useTranslation(null, {keyPrefix: "project"})
+    
+    const { isPending, isError, data: project, error } = useQuery({
+        queryKey: ['projects', id],
+        queryFn: () => getProject(id),
+    })
+
+    useEffect(() => {
+        setIsContextMenuOpen(false)
+    }, [project])
+
+    const patchMutation = useMutation({
+        mutationFn: (data) => {
+            return patchProject(id, data)
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['projects']})
+        },
+    }) // 수정 만드셈
+
+    const deleteMutation = useMutation({
+        mutationFn: () => {
+            return deleteProject(id)
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['projects']})
+        },
+    })
+
+    const handleAlert = () => {
+        return async () => {
+            setIsContextMenuOpen(false)
+            setIsAlertOpen(true)
+        }
+    }
+
+    const handleDelete = () => {
+        navigate(`/app/projects`)
+        deleteMutation.mutate()
+        toast.success(`"${project.name}" 프로젝트가 삭제되었습니다`)
+    }
+
+    if (isPending) {
+        return <div>로딩중...</div>
+        // 민영아.. 스켈레톤 뭐시기 만들어..
+    }
+
+    const drawers = project.drawers
 
     return (
     <>
@@ -25,17 +83,29 @@ const ProjectPage = () => {
             <PageTitle $color={"#" + project.color}>{project.name}</PageTitle>
             <Icons>
                 <FeatherIcon icon="plus" onClick={() => {setIsDrawerCreateOpen(true)}}/>
-                <FeatherIcon icon="more-horizontal"/>
+                <FeatherIcon icon="more-horizontal" onClick={handleToggleContextMenu(setSelectedButtonPosition, setIsContextMenuOpen)}/>
             </Icons>
         </TitleBox>
         {drawers && (drawers.length === 0) ? <NoDrawerText>{t("no_drawer")}</NoDrawerText> 
         : drawers.map((drawer) => (
             <Drawer key={drawer.id} project={project} drawer={drawer} color={project.color}/>
         ))}
-        { isDrawerCreateOpen &&
+        {isContextMenuOpen &&
+            <ContextMenu
+                items={[{"icon": "trash-2", "display": "Delete", "color": theme.project.danger, "func": handleAlert()}]}
+                selectedButtonPosition={selectedButtonPosition}
+            />
+        }
+        {isAlertOpen &&
+            <ModalPortal closeModal={() => {setIsAlertOpen(false)}}>
+                <DeleteAlert title={`"${project.name}" 프로젝트를`} onClose={() => {setIsAlertOpen(false)}} func={handleDelete}/>
+            </ModalPortal>
+        }
+        {isDrawerCreateOpen &&
             <ModalPortal closeModal={() => {setIsDrawerCreateOpen(false)}}>
                 <DrawerCreate onClose={() => {setIsDrawerCreateOpen(false)}}/>
-            </ModalPortal>}
+            </ModalPortal>
+        }
         <Outlet context={[id, project.color]} />
     </>
     )
