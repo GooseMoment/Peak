bd623381e01d9430e9646c9438c2bcd42ec0c8f2
Bombwhale <solkblte@icloud.com>
2024-07-20 17:14:21 +0900

    Add skeletonBreathingCSS

--- frontend/src/assets/skeleton.jsx
+++ frontend/src/assets/skeleton.jsx
@@ -20,3 +20,16 @@ export const skeletonCSS = p => css`
 
     animation: ${shineLines} 1.6s infinite linear;
 `
+
+const breathing = p => keyframes`
+    0%, 100% {
+        background-color: ${p.theme.skeleton.defaultColor};
+    }
+    40% {
+        background-color: ${p.theme.skeleton.shineColor};
+    }
+`
+
+export const skeletonBreathingCSS = css`
+    animation: ${p => breathing(p)} 2s infinite linear;
+`
