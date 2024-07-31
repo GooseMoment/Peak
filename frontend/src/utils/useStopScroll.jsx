c9fee40f4cb29db563e26bbcf97444dd3229a2a3
Bombwhale <solkblte@icloud.com>
2024-07-31 13:42:41 +0900

    Edit useStopScroll - do nothing if already overflow hidden

--- frontend/src/utils/useStopScroll.jsx
+++ frontend/src/utils/useStopScroll.jsx
@@ -2,6 +2,10 @@ import { useEffect } from "react"
 
 const useStopScroll = () => {
     useEffect(() => {
+        if (document.body.style.overflow === "hidden") {
+            return
+        }
+
         document.body.style.overflow = "hidden"
         document.querySelector("html").scrollTop = window.scrollY
         return () => document.body.style.overflow = null
