b559e912164e4b8025138a29b3caefbeffefdaa3
Bombwhale <solkblte@icloud.com>
2024-07-31 13:40:27 +0900

    Clean useStopScroll

--- frontend/src/utils/useStopScroll.jsx
+++ frontend/src/utils/useStopScroll.jsx
@@ -2,9 +2,9 @@ import { useEffect } from "react"
 
 const useStopScroll = () => {
     useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        document.querySelector('html').scrollTop = window.scrollY;
-        return () => document.body.style.overflow = null;
+        document.body.style.overflow = "hidden"
+        document.querySelector("html").scrollTop = window.scrollY
+        return () => document.body.style.overflow = null
     }, [])
 }
 
