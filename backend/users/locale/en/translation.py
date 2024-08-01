3e547a0fa02a96fcd8c6bf91d27aa497ddf814e1
Bombwhale <solkblte@icloud.com>
2024-08-01 15:58:37 +0900

    Add translations for send_mail_confirm_email

---
+++ backend/users/locale/en/translation.py
@@ -0,0 +1,11 @@
+translation = {
+    "mail_confirm_email": {
+        "subject": "Confirm your email",
+        "text_content": "To complete signing up at Peak, it is required to confirm your email. If you are @{username}, please go to the page linked below.\nLink: {link}",
+        "html_title": "Confirm Your Email",
+        "greeting": "Hi, @{username}.",
+        "click_button": "To complete your signing up at Peak, click the button below.",
+        "button_confirm": "I'm @{username}",
+        "copy_link": "or, you can just copy & paste the link below into your browser:",
+    },
+}
