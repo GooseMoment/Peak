297d4671d8d9ee5b8a6364e4117e0c1bd0c56370
minyoy <65756020+minyoy@users.noreply.github.com>
2024-08-02 21:08:58 +0900

    Task의 Time 기능 추가 (#189)
    * Fix #152
    
    * Fix Assigned CLine Color
    
    * Edit task.due_datetime display
    
    * Create Time Add functions
    
    * Add change success toast and time default value

--- frontend/src/components/tasks/utils/taskDate.jsx
+++ frontend/src/components/tasks/utils/taskDate.jsx
@@ -1,21 +1,29 @@
-const formatDate = (date) => {
-    const formattedDate = `${date?.getFullYear()}년 ${date?.getMonth()+1}월 ${date?.getDate()}일`
-    return formattedDate
-}
-
-const formatTime = (time) => {
-    const formattedTime = `${time?.getHours()}시 ${time?.getMinutes()}분`
-    return formattedTime
-}
+import { useClientLocale, useClientSetting } from "@utils/clientSettings"
 
 const taskDate = (task) => {
+    const [setting, ] = useClientSetting()
+    const locale = useClientLocale()
+
+    const formatDate = (date) => {
+        const formattedDate = date.toLocaleDateString(locale)
+        return formattedDate
+    }
+    
+    const formatTime = (time) => {
+        let formattedTime = ''
+        if (setting.time_as_24_hour)
+            formattedTime = time.toLocaleTimeString(locale, {hour12: false, hour: "numeric", minute: "numeric"})
+        else
+            formattedTime = time.toLocaleTimeString(locale, {hour12: true, hour: "numeric", minute: "numeric"})
+        return formattedTime
+    }
+
     const task_due = new Date(`${task.due_date}${task.due_time ? "T"+task.due_time : ""}`)
     const assigned_at_date = new Date(task.assigned_at)
-    const formatted_due_date = formatDate(task_due)
-    const formatted_due_time = task.due_time ? formatTime(task_due) : ""
+    const formatted_due_datetime = task.due_time ? formatDate(task_due) + ' ' + formatTime(task_due) : formatDate(task_due)
     const formatted_assigned_date = formatDate(assigned_at_date)
 
-    return {formatted_due_date, formatted_due_time, formatted_assigned_date}
+    return {formatted_due_datetime, formatted_assigned_date}
 }
 
 export default taskDate
