1ca5b26018293f468281bd5c07568dec285300b6
Bombwhale <solkblte@icloud.com>
2024-08-07 16:52:58 +0900

    Remove unused variables

--- frontend/src/pages/settings/Notifications.jsx
+++ frontend/src/pages/settings/Notifications.jsx
@@ -19,7 +19,7 @@ const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY
 const urlB64ToUint8Array = (base64String) => {
     const padding = "=".repeat((4 - (base64String.length % 4)) % 4)
     const base64 = (base64String + padding)
-        .replace(/\-/g, "+")
+        .replace(/-/g, "+")
         .replace(/_/g, "/")
     const rawData = atob(base64)
     const outputArray = new Uint8Array(rawData.length)
