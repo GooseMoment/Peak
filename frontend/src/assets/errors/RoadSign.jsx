9a9630307edc55b1a023e4229aaebe5afd6074d5
Bombwhale <solkblte@icloud.com>
2024-08-07 12:11:36 +0900

    Edit RoadSign - Decrease scale if mobile

--- frontend/src/assets/errors/RoadSign.jsx
+++ frontend/src/assets/errors/RoadSign.jsx
@@ -1,3 +1,5 @@
+import { ifMobile } from "@utils/useScreenType"
+
 import styled, { useTheme } from "styled-components"
 
 const RoadSign = ({text, spacing=10}) => {
@@ -8,7 +10,7 @@ const RoadSign = ({text, spacing=10}) => {
     const border = theme.grey
     const shadow = "#68686A"
 
-    return <svg width="356" height="258" viewBox="0 0 356 258" fill="none" xmlns="http://www.w3.org/2000/svg">
+    return <Svg width="356" height="258" viewBox="0 0 356 258" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="3.5" y="3.5" width="349" height="141" rx="28.5" stroke={border} strokeWidth="7"/>
         <rect x="22" y="22" width="312" height="104" rx="9" fill={inner} stroke={red} strokeWidth="14"/>
         <rect x="79" y="148" width="20" height="110" fill={border} />
@@ -20,8 +22,15 @@ const RoadSign = ({text, spacing=10}) => {
         <Text x="50%" y="95" textAnchor="middle">
             {[...text].map((letter, i) => <tspan key={letter + i} dx={i !== 0 ? spacing : 0}>{letter}</tspan>)}
         </Text>
-    </svg>
+    </Svg>
 }
+
+const Svg = styled.svg`
+
+    ${ifMobile} {
+        transform: scale(0.75);
+    }
+`
 
 const Text = styled.text`
     fill: ${p => p.theme.textColor};
