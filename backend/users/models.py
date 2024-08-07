8a3e4e0704bc4519a2a56672fcc47b5e117ee9bc
Bombwhale <solkblte@icloud.com>
2024-08-07 17:27:06 +0900

    Add PasswordRecoveryToken

--- backend/users/models.py
+++ backend/users/models.py
@@ -84,11 +84,32 @@ class EmailVerificationToken(models.Model):
     verified_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
-        return f"UserEmailVerficationToken for {self.user}"
+        return f"EmailVerficationToken for {self.user}"
     
     @property
     def link(self) -> str:
         return f"{settings.SCHEME}{settings.WEB_HOSTNAME}/sign/verification/?token={self.token.hex}"
 
     class Meta:
-        db_table = "user_email_verification_token"
+        db_table = "email_verification_tokens"
+
+class PasswordRecoveryToken(models.Model):
+    token = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"PasswordRecoveryToken for {self.user}"
+    
+    @property
+    def link(self) -> str:
+        return f"{settings.SCHEME}{settings.WEB_HOSTNAME}/sign/password-recovery/?token={self.token.hex}"
+
+    class Meta:
+        db_table = "password_recovery_tokens"
