0ed15b7e164f413f6e76f7ce44629cfce4bb1922
andless <andless2004@gmail.com>
2024-08-07 15:25:38 +0900

    Add recommend users list

--- frontend/src/pages/SocialExplorePage.jsx
+++ frontend/src/pages/SocialExplorePage.jsx
@@ -1,18 +1,76 @@
 import { useState } from "react"
-import styled from "styled-components"
+import { useQuery } from "@tanstack/react-query"
+import { styled, css } from "styled-components"
 
-import SocialCalendar from "@components/social/SocialCalendar";
-import SocialPageTitle from "@components/social/SocialPageTitle";
+import SocialCalendar from "@components/social/SocialCalendar"
+import SocialPageTitle from "@components/social/SocialPageTitle"
+import DailyLogPreview from "@components/social/DailyLogPreview"
+
+import { getExploreFeed } from "@api/social.api"
 
 const SocialExplorePage = () => {
-    const [activeFilter, setActiveFilter] = useState("all")
+
+    const { data: recommendUsers, isError: recommendUsersError } = useQuery({
+        queryKey: ['explore', 'recommend', 'users'],
+        queryFn: () => getExploreFeed(),
+        staleTime: 3*60*60*1000
+    })
 
     return <>
-    <SocialPageTitle active="explore"/>
-    
-    <SocialCalendar />
+        <SocialPageTitle active="explore"/>
 
+        <Wrapper>
+            <Container>
+                <DailyLogsPreviewContainer>
+                    {recommendUsers?Object.values(recommendUsers).map((dailyFollowerLog, index) => (
+                        <DailyLogPreview
+                            key={index}
+                            dailyLog={dailyFollowerLog} />
+                    )):null}
+                </DailyLogsPreviewContainer>
+            </Container>
+
+            <Container $isSticky={true}>
+                asdsd
+                {/* {dailyComment?<DailyLogDetail
+                    dailyComment={dailyComment}
+                    userLogDetails={dailyLogDetails}
+                    userLogsDetail={mockDailyFollowerLogsDetail[0]}
+                    user={user}
+                    saveDailyComment={dailyCommentMutation.mutate}
+                    day={selectedDate}
+                />:null} */}
+            </Container>
+        </Wrapper>
     </>
 }
+
+const Wrapper = styled.div`
+    display: flex;
+    gap: 5rem;
+`
+
+const Container = styled.div`
+    width: 50%;
+    min-width: 27.5rem;
+    ${props => props.$isSticky ? css`
+        /* align-self: flex-start; */
+        position: sticky;
+        top: 2.5rem;
+        gap: 0rem;
+    ` : css`
+        gap: 1rem;
+    `}
+    margin-bottom: auto;
+
+    padding: 0 1rem 0;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    overflow: hidden;
+`
+
+const DailyLogsPreviewContainer = styled.div``
 
 export default SocialExplorePage
