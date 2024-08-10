67e1bb92b4eb874681700ec2b7c769bab1fcf2b9
Bombwhale <solkblte@icloud.com>
2024-08-11 07:11:12 +0900

    Add VITE ENVs to Dockerfile

--- deploy/run-builder.sh
+++ deploy/run-builder.sh
@@ -1,2 +1,6 @@
-docker build -f ./"$1"/Dockerfile -t "$1"-build ./"$1" --env-file .env
+source .env
+docker build \
+    --build-arg VITE_API_BASEURL=$VITE_API_BASEURL \
+    --build-arg VITE_VAPID_PUBLIC_KEY=$VITE_VAPID_PUBLIC_KEY \
+    -f ./"$1"/Dockerfile -t "$1"-build ./"$1"
 docker run -v "$1"-dist:/"$1"/dist -t "$1"-build
