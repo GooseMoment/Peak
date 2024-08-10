1842df0c97604ebb40246dc1b2b24dc53ce0cebb
Bombwhale <solkblte@icloud.com>
2024-08-11 08:05:35 +0900

    Edit Dockerfile

--- deploy/run-builder.sh
+++ deploy/run-builder.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cp .env ./"$1"/.env
+cp .env ./frontend/.env
 docker build \
-    --build-arg VITE_API_BASEURL=$VITE_API_BASEURL \
-    --build-arg VITE_VAPID_PUBLIC_KEY=$VITE_VAPID_PUBLIC_KEY \
-    -f ./"$1"/Dockerfile -t "$1"-build ./"$1"
-docker run -v "$1"-dist:/"$1"/dist -t "$1"-build
+    -f ./frontend/Dockerfile -t frontend-build ./frontend
+docker run -v frontend-dist:/frontend/dist:ro -t frontend-build
+rm -f ./frontend/.env
