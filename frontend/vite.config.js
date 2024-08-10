da2e2ac3453aabfe4208a8386e7f59a869ae2248
Bombwhale <solkblte@icloud.com>
2024-08-11 07:23:43 +0900

    chore: Update vite.config.js
    Remove the base configuration option from vite.config.js. This change was made to simplify the configuration and align with the default behavior of Vite.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./", // https://github.com/vitejs/vite/discussions/5081#discussioncomment-1525248
     envDir: "..",
     server: {
         port: 3000,
