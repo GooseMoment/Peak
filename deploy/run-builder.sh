7757c292202911b3d5a17917a24356558dd57c51
Bombwhale <solkblte@icloud.com>
2024-08-13 18:16:50 +0900

    Add BUILD_DATE to run-builder

--- deploy/run-builder.sh
+++ deploy/run-builder.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+BUILD_DATE=$(date '+%Y-%m-%dT%H:%M:%S%z')
+
 docker build \
     -f ./frontend/Dockerfile \
+    --build-arg "BUILD_DATE=${BUILD_DATE}" \
     -t frontend-build ./frontend
+
 docker run \
     -v frontend-dist:/frontend/dist \
     -v ./.env:/.env:ro \
