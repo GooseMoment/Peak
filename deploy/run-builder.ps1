5980ea60f5509071f0f07e28f01aeb236f868695
Bombwhale <solkblte@icloud.com>
2024-08-13 17:03:24 +0900

    Remove args from run-builder

--- deploy/run-builder.ps1
+++ deploy/run-builder.ps1
@@ -1,4 +1,4 @@
-$name = $args[0]
+$name = "frontend"
 $directory = ".\" + "$name"
 $Dockerfile = "$directory" + "\Dockerfile"
 $imageName = "$name" + "-build"
@@ -6,5 +6,7 @@ $volumeName = "$name" + "-dist"
 $distDirectory = "/" + "$name" + "/dist"
 $volumeOption = $volumeName + ":" + $distDirectory
 
+Copy-Item .env -Destination .\frontend
 docker build -f $Dockerfile -t $imageName $directory
 docker run -v $volumeOption -t $imageName
+Remove-Item .\frontend\.env
