4c0dba5d9bd3e50b50894a307ab95dd0d6976592
minyoy <minyoy@naver.com>
2024-08-15 00:31:51 +0900

    Add getOverdueTasks and getTodayTasks

--- frontend/src/api/tasks.api.js
+++ frontend/src/api/tasks.api.js
@@ -1,71 +1,53 @@
 import client from "@api/client"
 
 export const getTasksByDrawer = async (drawerID, ordering, page) => {
-    try {
-        const res = await client.get(
-            `tasks?drawer=${drawerID}&ordering=${ordering}&page=${page}`,
-        )
-        return res.data
-    } catch (e) {
-        throw e
-    }
+    const res = await client.get(
+        `tasks?drawer=${drawerID}&ordering=${ordering}&page=${page}`,
+    )
+    return res.data
+}
+
+export const getOverdueTasks = async (filter_field, page) => {
+    const res = await client.get(`tasks/overdue?filter_field=${filter_field}&page=${page}`)
+    return res.data
+}
+
+export const getTodayTasks = async (page) => {
+    const res = await client.get(`tasks/today?page=${page}`)
+    return res.data
 }
 
 export const getTask = async (id) => {
-    try {
-        const res = await client.get(`tasks/${id}`)
-        return res.data
-    } catch (e) {
-        throw e
-    }
+    const res = await client.get(`tasks/${id}`)
+    return res.data
 }
 
 export const postTask = async (task) => {
-    try {
-        const res = await client.post("tasks/", task)
-        return res.status
-    } catch (e) {
-        throw e
-    }
+    const res = await client.post("tasks/", task)
+    return res.status
 }
 
 export const patchTask = async (id, edit) => {
-    try {
-        const res = await client.patch(`tasks/${id}`, edit)
-        return res.data
-    } catch (e) {
-        throw e
-    }
+    const res = await client.patch(`tasks/${id}`, edit)
+    return res.data
 }
 
 export const deleteTask = async (id) => {
-    try {
-        const res = await client.delete(`tasks/${id}`)
-        return res.data
-    } catch (e) {
-        throw e
-    }
+    const res = await client.delete(`tasks/${id}`)
+    return res.data
 }
 
 export const completeTask = async (id) => {
-    try {
-        const date = new Date()
-        const edit = {
-            completed_at: date.toISOString(),
-        }
-        return await patchTask(id, edit)
-    } catch (e) {
-        throw e
+    const date = new Date()
+    const edit = {
+        completed_at: date.toISOString(),
     }
+    return await patchTask(id, edit)
 }
 
 export const uncompleteTask = async (id) => {
-    try {
-        const edit = {
-            completed_at: null,
-        }
-        return await patchTask(id, edit)
-    } catch (e) {
-        throw e
+    const edit = {
+        completed_at: null,
     }
+    return await patchTask(id, edit)
 }
