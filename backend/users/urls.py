1bc12a649f1ae7c3cb56d1686a4d25b9e946a369
andless <129763673+andless2004@users.noreply.github.com>
2024-08-31 05:43:14 +0900

    Social: get_requesters (#222)
    * Add get_explore_feed
    
    * Add explore urls
    
    * Add getExploreFeed
    
    * Add recommend users list
    
    * Add LogsPreview
    
    * Rename model DailyComment -> Quote
    
    * Fix API with Rename DailyComment -> Quote
    
    * Rename model DailyComment -> Quote and Reconstruct components dir with fix error
    
    * Fix to work by emoji name instead of using id
    
    * Add Explore LogDetails
    
    * Add SearchBar at ExplorePage
    
    * Add get explore search results view with urls
    
    * Add API for GET ExploreSearchResults
    
    * Fix to show ExploreSearchResults
    
    * Fix SearchBar to search by enter key
    
    * Add SearchButton
    
    * Fix QuoteInput
    
    * Add StickyContainer
    
    * Replace username with index for PreviewBox key
    
    * Fix blank message of Quote
    
    * Explore로의 PR: notifications: DailyComment -> Quote 변경 (#208)
    
    * Edit createdummies to use Quote
    
    * Edit push.py and signals.py to use Quote
    
    * Fix ko translation for push
    
    * Fix NotificationPage to use Quote
    
    * Add Skeleton at LogDetails
    
    * Add LoadingCircle at ExploreFeed
    
    * Add LoadingCircle when search users
    
    * Fix to clear timer when enter key down
    
    * Add ExploreFeed with show RecommendUsers when there is no FoundUser
    
    * Fix to show recommend user
    
    * Add DailyLogDetail Pagination
    
    * Add get_privacy_filter
    
    * Add DailyLogDrawer and Task View with Pagination replace with DaiyLogDetails view
    
    * Add GET DailyLogDrawers and Tasks API
    
    * Fix to get DailyLogDrawers by useInfiniteQuery for pagination
    
    * Fix get DailyLogTasks
    
    * Fix to get DailyLogTasks with paginantion
    
    * Create migrations
    
    * Add to fetchNextPage by click button and scroll
    
    * Fix Details by pagnition
    
    * Add ExploreFeedView to get with pagination
    
    * Add ExploreSearchView
    
    * Rename getExploreFeed -> getExploreRecommend
    
    * Fix and Rename getExploreSearchResults -> getExploreFound
    
    * Fix to get FoundUsers with pagination
    
    * Fix ExplorePage SearchBar to active well
    
    * Remove unused import
    
    * Fix FollowView properly handle request status with checking authority and block
    
    * Fix FollowView GET to check authority
    
    * Fix FollowView to check authority
    
    * Fix PATCH FollowView
    
    * Fix PATCH FollowView logic
    
    * Fix DELETE FollowView with handle deleted_at
    
    * Fix GET followings followers to check status is accepted
    
    * Remove useless parts
    
    * Fix action when there is no searched result
    
    * Rename isFollowing -> is FollowingPage
    
    * Fix to use pagination in ExplorePage
    
    * Fix GET FollowView
    
    * Fix GET Follow API to return with status
    
    * Add FollowBox
    
    * Fix GET FollowView status
    
    * Fix FolllowView error status 401 -> 400
    
    * Add and Fix FollowBox
    
    * Fix DrawerBundle to use getProjectColor with useTheme
    
    * Edit PATCH FollowView
    
    * Fix GET FollowView to check block
    
    * Fix SimpleProfile color
    
    * Add placeholder at Explore SearchInput
    
    * Fix import path
    
    * Fix FollowingPage to display only "next" or "no_more"
    
    * Edit DrawerBundle color to use constant
    
    * Fix GET Follow API
    
    * Fix LogDetails to use FollowButton
    
    * Add get_requesters
    
    * Add urls for get_requesters
    
    * Add GET RequestersByUser API
    
    * Fix get followers, followings, requesters view to check block
    
    * Fix get_requesters authority
    
    * Rename variables
    
    * Fix getRequestersByUser API
    
    ---------
    
    Co-authored-by: JedBeom <solkblte@icloud.com>

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 from user_setting.views import UserSettingDetail
 from projects.views import UserProjectList
-from social.views import get_followers, get_followings, get_blocks
+from social.views import get_followers, get_followings, get_blocks, get_requesters
 from knox import views as knox_views
 
 urlpatterns = [
@@ -22,6 +22,7 @@ urlpatterns = [
     path("users/@<str:username>/", views.UserDetail.as_view()),
     path("users/@<str:username>/followers/", get_followers),
     path("users/@<str:username>/followings/", get_followings),
+    path("users/@<str:username>/requesters/", get_requesters),
     path("users/@<str:username>/blocks/", get_blocks),
     path("users/@<str:username>/projects/", UserProjectList.as_view()),
 ]
