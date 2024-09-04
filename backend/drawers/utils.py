446f2bf8d93ef5ba3281de600b12366ef2a15b80
minyoy <minyoy@naver.com>
2024-09-05 01:37:13 +0900

    Fix DoesNotExist Error

--- backend/drawers/utils.py
+++ backend/drawers/utils.py
@@ -1,27 +1,33 @@
 from django.db.models import F
+from django.core.exceptions import ObjectDoesNotExist
 
 def reorder_tasks(drawer_tasks, dragged_order, target_order, closest_edge):
     if dragged_order == target_order:
         return
     
-    dragged_task = drawer_tasks.get(order=dragged_order)
+    try:
+        dragged_task = drawer_tasks.get(order=dragged_order)
+    except (ObjectDoesNotExist):
+        pass
+    else:
+        dragged_task.order = target_order
+        if dragged_order < target_order:
+            tasks_to_shift = drawer_tasks.filter(order__lte=target_order)
+            if (tasks_to_shift):
+                tasks_to_shift.update(order=F('order') - 1)
 
-    dragged_task.order = target_order
-    if dragged_order < target_order:
-        tasks_to_shift = drawer_tasks.filter(order__lte=target_order)
-        tasks_to_shift.update(order=F('order') - 1)
+            if closest_edge == "top":
+                dragged_task.order = target_order - 1
+            
+        elif dragged_order > target_order:
+            tasks_to_shift = drawer_tasks.filter(order__gte=target_order)
+            if (tasks_to_shift):
+                tasks_to_shift.update(order=F('order') + 1)
 
-        if closest_edge == "top":
-            dragged_task.order = target_order - 1
-        
-    elif dragged_order > target_order:
-        tasks_to_shift = drawer_tasks.filter(order__gte=target_order)
-        tasks_to_shift.update(order=F('order') + 1)
+            if closest_edge == "bottom":
+                dragged_task.order = target_order + 1
 
-        if closest_edge == "bottom":
-            dragged_task.order = target_order + 1
-
-    dragged_task.save()
+        dragged_task.save()
 
 def normalize_drawer_order(drawer_tasks):
     ordered_tasks = drawer_tasks.order_by('order')
