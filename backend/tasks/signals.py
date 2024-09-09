2610346ba2bd4ea6d128c953e86a83081f408725
minyoy <minyoy@naver.com>
2024-09-09 23:31:55 +0900

    Edit set_new_task_order function

--- backend/tasks/signals.py
+++ backend/tasks/signals.py
@@ -2,15 +2,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Task
-from drawers.models import Drawer
 
 @receiver(post_save, sender=Task)
-def set_task_order(sender, instance: Task=None, created=False, **kwargs):
+def set_new_task_order(sender, instance: Task=None, created=False, **kwargs):
     if not created:
         return
 
-    drawer_in_task = Drawer.objects.get(id=instance.drawer.id)
-    instance.order = drawer_in_task.uncompleted_task_count + drawer_in_task.completed_task_count
-    print(instance.order)
+    tasks_in_same_drawer = Task.objects.filter(drawer=instance.drawer.id)
+    instance.order = tasks_in_same_drawer.count() - 1
 
     instance.save()
