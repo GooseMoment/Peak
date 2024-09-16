a36d22eedc3538889410c284c117fc85ca6732d6
Bombwhale <solkblte@icloud.com>
2024-09-16 01:01:01 -0400

    Add OpenWithSafari

---
+++ frontend/src/components/home/OpenWithSafari.jsx
@@ -0,0 +1,10 @@
+import Module, { CenteredText, Title } from "./Module"
+
+const OpenWithSafari = () => {
+    return <Module>
+        <Title underline>Open with Safari</Title>
+        <CenteredText>With Safari, you can install Peak as an app.</CenteredText>
+    </Module>
+}
+
+export default OpenWithSafari
