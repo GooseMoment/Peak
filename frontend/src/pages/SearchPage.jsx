4f65b7370d355a593d046d6ba422263ea8e39b5f
andless <andless2004@gmail.com>
2024-09-17 00:31:41 +0900

    Add filters type

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -7,26 +7,32 @@ import SearchBar from "@components/search/SearchBar"
 const initialFilterGroup = {
     project: {
         name: "Project",
+        type: "text",
         value: null,
     },
     drawer: {
         name: "Drawer",
+        type: "text",
         value: null,
     },
     assignedAt: {
         name: "Assign",
+        type: "date",
         value: null,
     },
     completedAt: {
         name: "Completed",
+        type: "date",
         value: null,
     },
     privacy: {
         name: "Privacy",
+        type: "text",
         value: null,
     },
     enableMemoSearch: {
         name: "Memo",
+        type: "text",
         value: null,
     },
 }
