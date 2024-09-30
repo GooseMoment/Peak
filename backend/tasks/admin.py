f87af645c1f62ae37239a39056586232d3e5e69b
Bombwhale <solkblte@icloud.com>
2024-09-29 21:49:02 -0400

    Edit TaskAdmin - Add assigned_at & due_tz to fields

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -18,7 +18,7 @@ class TaskAdmin(admin.ModelAdmin):
         (
             "Date & Time",
             {
-                "fields": ["completed_at", "due_date", "due_time"],
+                "fields": ["completed_at", "assigned_at", "due_date", "due_time", "due_tz"],
             },
         ),
         fieldset_base,
