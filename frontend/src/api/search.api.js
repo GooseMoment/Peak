f686ad07f848ff460364e35ad22a60b79f17c330
andless <andless2004@gmail.com>
2024-10-04 16:32:33 +0900

    Fix Search API

--- frontend/src/api/search.api.js
+++ frontend/src/api/search.api.js
@@ -1,7 +1,20 @@
 import client from "@api/client"
 
-export const getSearchResults = async (query, cursor) => {
-    const res = await client.get(`search/?${query}&cursor=${cursor}`)
+export const getSearchResults = async (filters, cursor) => {
+    const searchparam = new URLSearchParams()
+
+    Object.entries(filters).map(([filterName, filterBody]) => {
+        if (filterBody.value && filterBody.type === "date") {
+            searchparam.set(
+                filterName,
+                filterBody.value.startDate + "to" + filterBody.value.endDate,
+            )
+        } else {
+            searchparam.set(filterName, filterBody.value)
+        }
+    })
+
+    const res = await client.get(`search/?${searchparam}&cursor=${cursor}`)
 
     return res.data
 }
