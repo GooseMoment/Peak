10163e036d50303b44ef69e906c7d2550326c0d3
Bombwhale <solkblte@icloud.com>
2024-10-04 12:30:50 -0400

    Edit Header - Add my profile

--- frontend/src/components/home/Header.jsx
+++ frontend/src/components/home/Header.jsx
@@ -1,18 +1,35 @@
 import { Link } from "react-router-dom"
 
+import { useQuery } from "@tanstack/react-query"
 import styled from "styled-components"
 
 import PageTitle from "@components/common/PageTitle"
 
+import { getMe } from "@api/users.api"
+
 import { ifMobile } from "@utils/useScreenType"
+
+import { skeletonBreathingCSS } from "@assets/skeleton"
 
 import FeatherIcon from "feather-icons-react"
 
 const Header = () => {
+    const { data: me, isFetching } = useQuery({
+        queryKey: ["users", "me"],
+        queryFn: () => getMe(),
+    })
+
     return (
         <Frame>
             <PageTitle>Home</PageTitle>
             <HeaderIcons>
+                {isFetching ? (
+                    <HeaderProfileLoading />
+                ) : (
+                    <Link to={`/app/users/@${me.username}`}>
+                        <HeaderProfile src={me.profile_img} />
+                    </Link>
+                )}
                 <Link to="/app/notifications">
                     <FeatherIcon icon="bell" />
                 </Link>
@@ -46,6 +63,25 @@ const HeaderIcons = styled.div`
     ${ifMobile} {
         display: flex;
     }
+`
+
+const HeaderProfile = styled.img`
+    aspect-ratio: 1/1;
+    height: 1.5em;
+    width: 1.5em;
+    border-radius: 50%;
+    padding: 0 0.25em;
+`
+
+const HeaderProfileLoading = styled.div`
+    aspect-ratio: 1/1;
+    height: 1.5em;
+    width: 1.5em;
+    border-radius: 50%;
+    margin: 0 0.25em;
+    box-sizing: border-box;
+
+    ${skeletonBreathingCSS}
 `
 
 export default Header
