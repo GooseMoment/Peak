56f103fa291da960e81b00ee22e1e9633d7d4aae
Bombwhale <solkblte@icloud.com>
2024-10-05 00:44:27 -0400

    Add HeartButton

--- frontend/src/pages/AnnouncementDetailPage.jsx
+++ frontend/src/pages/AnnouncementDetailPage.jsx
@@ -3,15 +3,22 @@ import { useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import styled, { useTheme } from "styled-components"
 
+import HeartButton from "@components/announcements/HeartButton"
 import { LoaderCircleFull } from "@components/common/LoaderCircle"
 import PageBack from "@components/common/PageBack"
 import PageTitle from "@components/common/PageTitle"
 
 import { getAnnouncement } from "@api/announcements.api"
 
+import { useClientLocale, useClientTimezone } from "@utils/clientSettings"
+
+import { DateTime } from "luxon"
+
 const AnnouncementDetailPage = () => {
     const { id } = useParams()
     const theme = useTheme()
+    const tz = useClientTimezone()
+    const locale = useClientLocale()
 
     if (theme.type === "light") {
         import("github-markdown-css/github-markdown-light.css")
@@ -35,13 +42,26 @@ const AnnouncementDetailPage = () => {
         <>
             <PageBack defaultTo="/app/announcements">Back to the list</PageBack>
             <PageTitle>{data.title}</PageTitle>
+            <ContentDate>
+                {DateTime.fromISO(data.created_at)
+                    .setLocale(locale)
+                    .setZone(tz)
+                    .toLocaleString()}
+            </ContentDate>
             <Content
                 className="markdown-body announcement_detail_content"
                 dangerouslySetInnerHTML={{ __html: data.content }}
             />
+            <HeartButton announcement={data} />
         </>
     )
 }
+
+const ContentDate = styled.time`
+    display: block;
+    font-size: 0.75em;
+    margin-bottom: 1em;
+`
 
 const Content = styled.div`
     background-color: ${(p) => p.theme.thirdBackgroundColor};
