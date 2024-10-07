e941bf26a0c1ea64a4fad13b6f8cd0523161dad8
minyoy <minyoy@naver.com>
2024-10-08 00:11:23 +0900

    Add choices to due_type

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -25,6 +25,14 @@ class Repeat(Base):
         db_table = "repeats"
 
 class Task(Base, PrivacyMixin):
+    DUE_DATE = "due_date"
+    DUE_DATETIME = "due_datetime"
+
+    TASK_DUE_TYPE_CHOICES = [
+        (DUE_DATE, "due_date"),
+        (DUE_DATETIME, "due_datetime"),
+    ]
+
     name = models.CharField(max_length=128)
     completed_at = models.DateTimeField(null=True, blank=True)
     drawer = models.ForeignKey(
@@ -32,7 +40,7 @@ class Task(Base, PrivacyMixin):
         on_delete=models.CASCADE,
         related_name='tasks'
     )
-    due_type = models.CharField(max_length=12, null=True, blank=True)
+    due_type = models.CharField(choices=TASK_DUE_TYPE_CHOICES, max_length=12, null=True, blank=True)
     due_date = models.DateField(null=True, blank=True)
     due_datetime = models.DateTimeField(null=True, blank=True)
     assigned_at = models.DateField(null=True, blank=True)
