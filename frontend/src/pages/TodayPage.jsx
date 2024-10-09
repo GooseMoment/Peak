c025fbf80d257495238b75d33486ee79c9949a7f
minyoy <minyoy@naver.com>
2024-10-10 00:21:52 +0900

    Add mobile view for TodayPage

--- frontend/src/pages/TodayPage.jsx
+++ frontend/src/pages/TodayPage.jsx
@@ -237,26 +237,36 @@ const OverdueTasksBlock = styled.div`
     border: 1.8px solid ${(p) => p.theme.project.borderColor};
     border-radius: 15px;
     margin: 1.4em 0.5em;
-    padding: 1.5em 1em;
+    padding: 1.5em 0.8em;
+    
+    ${ifMobile} {
+        margin: 0;
+        padding: 1em;
+    }
 `
 
 const OverdueTitle = styled.div`
     font-size: 1.2em;
     font-weight: bold;
-    margin-left: 1em;
+    margin-left: 0.6em;
 `
 
 const CollapseButtonBlock = styled.div`
     float: right;
     cursor: pointer;
     margin-top: 0.1em;
-    margin-right: 0.5em;
+    margin-right: 0.4em;
 `
 
 const OverdueTaskBox = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+
+    ${ifMobile} {
+        flex-direction: column;
+        align-items: flex-start;
+    }
 `
 
 const Icons = styled(FlexCenterBox)`
@@ -266,12 +276,16 @@ const Icons = styled(FlexCenterBox)`
         stroke: ${(p) => p.theme.textColor};
         cursor: pointer;
     }
+
+    ${ifMobile} {
+        margin: 0em 1.2em 1em;
+    }
 `
 
 const FilterButtonBox = styled.div`
     display: flex;
     gap: 0.8em;
-    margin: 1em 1.2em 0em;
+    margin: 1em 0.6em 0em;
 `
 
 const FilterButton = styled.div`
