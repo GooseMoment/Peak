d3e9f571a2de26a28002f86da4a04243b696c70e
Bombwhale <solkblte@icloud.com>
2024-10-31 18:05:05 -0400

    Remove unused imports

--- backend/peak_auth/views.py
+++ backend/peak_auth/views.py
@@ -9,7 +9,7 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
-from rest_framework.decorators import api_view, permission_classes, throttle_classes
+from rest_framework.decorators import throttle_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.throttling import AnonRateThrottle
 
