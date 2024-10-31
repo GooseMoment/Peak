0ae622ee43ce6db6c7a18de0238e767d53b514ae
Bombwhale <solkblte@icloud.com>
2024-10-31 17:50:11 -0400

    Edit SIGN_UP error codes

--- backend/peak_auth/exceptions.py
+++ backend/peak_auth/exceptions.py
@@ -6,52 +6,52 @@ from api.exceptions import APIException
 class UserAlreadyAuthenticated(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "The user is already authenticated."
-    default_code = "SIGNUP_USER_ALREADY_AUTHENTICATED"
+    default_code = "USER_ALREADY_AUTHENTICATED"
 
 
 class RequiredFieldMissing(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Some fields are missing."
-    default_code = "SIGNUP_REQUIRED_FIELD_MISSING"
+    default_code = "REQUIRED_FIELD_MISSING"
 
 
 class EmailInvalid(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Invalid email format."
-    default_code = "SIGNUP_EMAIL_INVALID"
+    default_code = "EMAIL_INVALID"
 
 
 class UsernameInvalidLength(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Username should be between 4 and 15."
-    default_code = "SIGNUP_USERNAME_INVALID_LENGTH"
+    default_code = "USERNAME_INVALID_LENGTH"
 
 
 class UsernameInvalidFormat(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Username should contain alphanumberics and underscore only."
-    default_code = "SIGNUP_USERNAME_INVALID_FORMAT"
+    default_code = "USERNAME_INVALID_FORMAT"
 
 
 class UsernameDuplicate(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Duplicate username."
-    default_code = "SIGNUP_USERNAME_DUPLICATE"
+    default_code = "USERNAME_DUPLICATE"
 
 
 class PasswordInvalid(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Password should be longer than 8."
-    default_code = "SIGNUP_PASSWORD_INVALID"
+    default_code = "PASSWORD_INVALID"
 
 
 class UnknownError(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = "Unknown error occuered."
-    default_code = "SIGNUP_UNKNOWN_ERROR"
+    default_code = "UNKNOWN_ERROR"
 
 
 class EmailNotSent(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = "The requested email was not sent."
-    default_code = "SIGNUP_EMAIL_NOT_SENT"
+    default_code = "EMAIL_NOT_SENT"
