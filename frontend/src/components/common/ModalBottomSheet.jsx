c573409945c853412f1d0c61cdf121cef59d2e72
minyoy <65756020+minyoy@users.noreply.github.com>
2024-11-05 14:39:53 +0900

    Add descriptive comment

--- frontend/src/components/common/ModalBottomSheet.jsx
+++ frontend/src/components/common/ModalBottomSheet.jsx
@@ -6,7 +6,7 @@ import FeatherIcon from "feather-icons-react"
 import { BottomSheet } from "react-spring-bottom-sheet"
 import "react-spring-bottom-sheet/dist/style.css"
 
-export const Header = ({ title = null, handleBack, closeSheet }) => {
+export const Header = ({ title = null, handleBack = ()=>{}, closeSheet }) => {
     return (
         <HeaderBox>
             {handleBack ? (
@@ -73,6 +73,7 @@ const HeaderBox = styled.div`
     }
 `
 
+// Same as chevron-left icon size
 const EmptyBox = styled.div`
     width: 17.6px;
     height: 17.6px;
