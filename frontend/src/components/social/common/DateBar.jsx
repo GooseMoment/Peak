a6be5e2146ba9dc34383444253937626658802af
andless <andless2004@gmail.com>
2025-02-11 03:36:38 +0900

    Add open/close functionality for calendar at DateBar

--- frontend/src/components/social/common/DateBar.jsx
+++ frontend/src/components/social/common/DateBar.jsx
@@ -1,9 +1,14 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import styled from "styled-components"
 
-import { useClientLocale } from "@utils/clientSettings"
+import CommonCalendar from "@components/common/CommonCalendar"
+import MildButton from "@components/common/MildButton"
 
+import { useClientLocale } from "@utils/clientSettings"
+import { ifMobile } from "@utils/useScreenType"
+
+import FeatherIcon from "feather-icons-react"
 import { DateTime } from "luxon"
 import { useTranslation } from "react-i18next"
 
@@ -11,13 +16,107 @@ const DateBar = ({ selectedDate, setSelectedDate }) => {
     const locale = useClientLocale()
     const { t } = useTranslation("", { keyPrefix: "social.date_bar" })
 
-    const [date, setDate] = useState(
-        selectedDate ? DateTime.fromISO(selectedDate) : DateTime.now(),
-    )
+    // TODO: selectedDate를 Datetime형으로 바꾼 이후 다시 수정
+    const displayDate = () => {
+        const date = DateTime.fromISO(selectedDate)
+        return date.setLocale(locale).toFormat(t("date_format"))
+    }
 
-    return <Frame>{date.setLocale(locale).toFormat(t("date_format"))}</Frame>
+    const handleDate = (diff) => {
+        const date = DateTime.fromISO(selectedDate)
+        const newDate = date.plus({days: diff}) 
+        setSelectedDate(newDate.setZone('utc').toISO())
+    }
+
+    const [isCalendarOpen, setIsCalendarOpen] = useState(false)
+
+    return (
+        <Frame>
+            <NavBar>
+                <NavButton $buttonSize={1} onClick={() => handleDate(-7)}>
+                    <FeatherIcon icon="chevrons-left" />
+                </NavButton>
+                <NavButton $buttonSize={2} onClick={() => handleDate(-1)}>
+                    <FeatherIcon icon="chevron-left" />
+                </NavButton>
+                <DateBox
+                    onClick={() => {
+                        setIsCalendarOpen((prev) => !prev)
+                    }}>
+                    {displayDate()}
+                </DateBox>
+                <NavButton $buttonSize={2} onClick={() => handleDate(1)}>
+                    <FeatherIcon icon="chevron-right" />
+                </NavButton>
+                <NavButton $buttonSize={1} onClick={() => handleDate(7)}>
+                    <FeatherIcon icon="chevrons-right" />
+                </NavButton>
+            </NavBar>
+
+            {isCalendarOpen && (
+                <CalendarWrapper>
+                    <CommonCalendar
+                        isRangeSelectMode={false}
+                        selectedStartDate={selectedDate}
+                        setSelectedStartDate={setSelectedDate}
+                    />
+                </CalendarWrapper>
+            )}
+        </Frame>
+    )
 }
 
-const Frame = styled.div``
+const Frame = styled.div`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const NavBar = styled.div`
+    margin: 0.5em;
+    width: 100%;
+    max-width: 35rem;
+    height: 1.2em;
+
+    font-size: 1.2em;
+    display: flex;
+    flex-direction: row;
+`
+
+const NavButton = styled(MildButton)`
+    height: 100%;
+    min-width: ${(props) => props.$buttonSize}em;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & svg {
+        top: unset;
+        margin-right: unset;
+    }
+`
+
+const DateBox = styled.div`
+    min-width: 7em;
+    flex-grow: 1;
+
+    text-align: center;
+    font-weight: bold;
+`
+
+const CalendarWrapper = styled.div`
+    margin: 0 auto;
+    width: 80%;
+    max-width: 35rem;
+
+    ${ifMobile} {
+        min-width: 20rem;
+
+        font-size: 0.9em;
+    }
+`
 
 export default DateBar
