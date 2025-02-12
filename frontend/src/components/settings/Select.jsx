6bf7cc34742a20d8bdc0b936912ca380f52b9fde
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-02-12 13:27:48 -0500

    Run format:write with new prettier

--- frontend/src/components/settings/Select.jsx
+++ frontend/src/components/settings/Select.jsx
@@ -56,7 +56,8 @@ const StyledSelect = styled.select`
     -webkit-appearance: none;
     -moz-appearance: none;
 
-    background-image: linear-gradient(45deg, transparent 50%, gray 50%),
+    background-image:
+        linear-gradient(45deg, transparent 50%, gray 50%),
         linear-gradient(135deg, gray 50%, transparent 50%),
         linear-gradient(to right, #ccc, #ccc);
     background-position:
