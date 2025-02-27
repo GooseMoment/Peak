e9b7ce67e005386e62fbbed3cf6dbefec36f7ff2
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-02-27 16:37:58 -0500

    Fix diff in translation.json

--- frontend/src/components/sign/forms.jsx
+++ frontend/src/components/sign/forms.jsx
@@ -147,7 +147,7 @@ export const TOTPAuthForm = () => {
             e?.preventDefault()
 
             if (totpCode.length < 6) {
-                return toast.error(t("enter_6_digits"))
+                return toast.error(t("enter_6_digit"))
             }
 
             return authTOTP("totp", totpCode)
