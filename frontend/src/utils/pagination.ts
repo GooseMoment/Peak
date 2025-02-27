909f20c93ad8c180fc2cd857b90a2b5204f53626
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-02-27 11:43:51 -0500

    Fix pagination argument types

--- frontend/src/utils/pagination.ts
+++ frontend/src/utils/pagination.ts
@@ -1,27 +1,19 @@
-export const getPageFromURL = (url: null | string): null | string => {
-    if (!url) return null
-
-    let u: URL
-    if (typeof url === "string") {
-        u = new URL(url)
-    } else {
-        u = url
+export const getPageFromURL = (url: null | string | URL): null | string => {
+    if (url === null || url === "") {
+        return url
     }
 
+    const u = new URL(url)
     const page = u.searchParams.get("page")
     return page
 }
 
-export const getCursorFromURL = (url: null | string): null | string => {
-    if (!url) return null
-
-    let u: URL
-    if (typeof url === "string") {
-        u = new URL(url)
-    } else {
-        u = url
+export const getCursorFromURL = (url: null | string | URL): null | string => {
+    if (url === null || url === "") {
+        return url
     }
 
+    const u = new URL(url)
     const cursor = u.searchParams.get("cursor")
     return cursor
 }
