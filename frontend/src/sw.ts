a69ed4715cfda3c6822b57506d6e5b6a4d42cbc9
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-02-28 23:36:10 -0500

    Move publicservice-worker.js to src/sw.ts

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -1,14 +1,16 @@
-console.log("Service Worker start")
+/// <reference lib="webworker" />
+
+declare const self: ServiceWorkerGlobalScope
 
 self.addEventListener("push", function (event) {
-    const data = event?.data?.json()
+    const data = event.data?.json()
     if (data) {
         self.registration.showNotification(data.title, data)
     }
 })
 
 self.addEventListener("notificationclick", function (event) {
-    const url = event?.notification?.data?.click_url
+    const url = event.notification.data?.click_url
 
     event.waitUntil(
         self.clients.matchAll({ type: "window" }).then((clients) => {
@@ -26,3 +28,6 @@ self.addEventListener("notificationclick", function (event) {
         }),
     )
 })
+
+// https://github.com/microsoft/TypeScript/issues/14877#issuecomment-402743582
+export default null
