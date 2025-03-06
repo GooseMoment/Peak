432985f5d6f9d3d07b130b207f138d1c0077ee17
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-03-06 17:12:50 -0500

    Make users.api typed

---
+++ frontend/src/api/users.api.ts
@@ -0,0 +1,53 @@
+import client, { getCurrentUsername, setCurrentUsername } from "@api/client"
+
+export interface User {
+    username: string
+    display_name: string
+    followings_count: number
+    followers_count: number
+    profile_img: string
+    bio: string
+    email: string
+    header_color: string
+    is_me: boolean
+}
+
+export const getMe = async () => {
+    const res = await client.get<User>("users/me")
+    setCurrentUsername(res.data.username)
+    return res.data
+}
+
+export const getUserByUsername = async (username: User["username"]) => {
+    const res = await client.get<User>(`users/@${username}/`)
+    return res.data
+}
+
+export const patchUser = async (data: Partial<User>) => {
+    const username = getCurrentUsername()
+    const res = await client.patch<User>(`users/@${username}/`, data)
+    return res.status
+}
+
+export const patchPassword = async (
+    current_password: string,
+    new_password: string,
+) => {
+    await client.patch("users/me/password/", {
+        current_password,
+        new_password,
+    })
+}
+
+export const uploadProfileImg = async (formData: FormData) => {
+    const res = await client.post("users/me/profile_img/", formData)
+    return res.status
+}
+
+export const getBlocks = async (page: number) => {
+    const username = getCurrentUsername()
+    const res = await client.get<User[]>(`users/@${username}/blocks/`, {
+        params: { page },
+    })
+    return res.data
+}
