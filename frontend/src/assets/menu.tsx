70c8fe10a81eec6628d84aee0cc67fa0cc6d22e2
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-03-10 17:51:57 -0400

    Fix Menu style

--- frontend/src/assets/menu.tsx
+++ frontend/src/assets/menu.tsx
@@ -8,13 +8,14 @@ import "@szhsin/react-menu/dist/index.css"
 import "@szhsin/react-menu/dist/transitions/zoom.css"
 
 export const Menu = styled(DefaultMenu)`
+    padding-right: 13px;
+
     & ul {
         position: absolute;
         background-color: ${(p) => p.theme.secondBackgroundColor};
         color: ${(p) => p.theme.textColor};
         border-radius: 10px;
         box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
-        padding-right: 13px !important;
         font-size: 0.85em;
     }
 `
