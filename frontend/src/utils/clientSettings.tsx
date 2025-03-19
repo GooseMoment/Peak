bc497df5648d9614c248f7d36eb6971f17c02525
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-03-18 23:31:31 -0700

    Make clientSettings typed

--- frontend/src/utils/clientSettings.tsx
+++ frontend/src/utils/clientSettings.tsx
@@ -1,37 +1,57 @@
-import { createContext, useContext, useMemo, useState } from "react"
+import { ReactNode, createContext, useContext, useMemo, useState } from "react"
+
+import { LightDark } from "styled-components"
+
+import type { WebPushSubscription } from "@api/notifications.api"
 
 import themes from "@assets/themes"
 
 const KEY_CLIENT_SETTINGS = "client_settings"
 
-const defaultSettings = {
-    // general
-    startpage: "home",
-    delete_task_after_alert: true,
+interface Setting {
+    startpage: "home" | "today"
+    delete_task_after_alert: boolean
 
     // Languages & Region
     //      locale = language + "-" + region
-    locale: "system",
-    timezone: "system",
+    locale: string
+    timezone: string
 
-    start_of_the_week_monday: false,
-    time_as_24_hour: false,
+    start_of_the_week_monday: boolean
+    time_as_24_hour: boolean
 
     // Notifications
-    push_notification_subscription: null,
-    push_notification_excluded_types: [],
-    play_notification_sound: true,
+    push_notification_subscription?: WebPushSubscription["id"]
+    push_notification_excluded_types?: WebPushSubscription["excluded_types"]
+    play_notification_sound: boolean
 
     // Appearance
+    theme: "system" | LightDark
+    main_width: "2rem" | "5rem" | "7rem"
+    close_sidebar_on_startup: boolean
+}
+
+const defaultSettings: Setting = {
+    startpage: "home",
+    delete_task_after_alert: true,
+    locale: "system",
+    timezone: "system",
+    start_of_the_week_monday: false,
+    time_as_24_hour: false,
+    push_notification_subscription: undefined,
+    push_notification_excluded_types: [],
+    play_notification_sound: true,
     theme: "system",
     main_width: "5rem",
     close_sidebar_on_startup: false,
 }
 
 export const getClientSettings = () => {
+    const savedSettings = localStorage.getItem(KEY_CLIENT_SETTINGS)
+
     try {
         return (
-            JSON.parse(localStorage.getItem(KEY_CLIENT_SETTINGS)) ||
+            (savedSettings && (JSON.parse(savedSettings) as Setting)) ||
             defaultSettings
         )
     } catch {
@@ -39,36 +59,55 @@ export const getClientSettings = () => {
     }
 }
 
-export const setClientSettingsByName = (name, value) => {
-    let settings = getClientSettings()
+export const setClientSettingsByName = <
+    K extends keyof Setting,
+    V extends Setting[K],
+>(
+    name: K,
+    value: V,
+) => {
+    const settings = getClientSettings()
     settings[name] = value
     localStorage.setItem(KEY_CLIENT_SETTINGS, JSON.stringify(settings))
 }
 
 export const initClientSettings = () => {
     let settings = getClientSettings()
-    if (!settings?.theme) {
+    if (!settings.theme) {
         settings = defaultSettings
     }
     settings = Object.assign({}, defaultSettings, settings)
     localStorage.setItem(KEY_CLIENT_SETTINGS, JSON.stringify(settings))
 }
 
-const ClientSettingContext = createContext()
+function dummyUpdateSetting<K extends keyof Setting, V extends Setting[K]>(
+    _k: K,
+    _v: V,
+) {}
 
-export const ClientSettingProvider = ({ children }) => {
+const ClientSettingContext = createContext([
+    defaultSettings,
+    dummyUpdateSetting,
+] as [Setting, typeof dummyUpdateSetting])
+
+export const ClientSettingProvider = ({
+    children,
+}: {
+    children?: ReactNode
+}) => {
     const [setting, setSetting] = useState(getClientSettings())
 
     const updateSetting = useMemo(
-        () => (key, val) => {
-            setClientSettingsByName(key, val)
-            setSetting(getClientSettings())
-        },
+        () =>
+            <K extends keyof Setting, V extends Setting[K]>(key: K, val: V) => {
+                setClientSettingsByName(key, val)
+                setSetting(getClientSettings())
+            },
         [],
     )
 
     const providerValue = useMemo(
-        () => [setting, updateSetting],
+        () => [setting, updateSetting] as [Setting, typeof dummyUpdateSetting],
         [setting, updateSetting],
     )
 
@@ -83,8 +122,10 @@ export const useClientSetting = () => {
     return useContext(ClientSettingContext)
 }
 
-const getTimezone = (settingTz) => {
+const getTimezone = (settingTz: string) => {
+    // if settingTz is "system"
     if (settingTz === defaultSettings["timezone"]) {
+        // return current timezone
         return new window.Intl.DateTimeFormat().resolvedOptions().timeZone
     }
 
@@ -121,7 +162,7 @@ export const getClientTimezone = () => {
     return tz
 }
 
-export const useClientTheme = (systemTheme) => {
+export const useClientTheme = (systemTheme: "system" | LightDark) => {
     const [setting] = useContext(ClientSettingContext)
     const theme = setting?.theme === "system" ? systemTheme : setting?.theme
 
