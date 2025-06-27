04b1e314b48586a7a7c3cc085cab70617f920bc0
minyoy <65756020+minyoy@users.noreply.github.com>
2025-06-28 00:58:05 +0900

    ProjectListPage에 Drag and Drop 적용 (#380)
    * Add react-dnd library
    
    * Fix Projects to load in order
    
    * Apply Drag and Drop to ProjectListPage
    
    * Run ruff
    
    * Fix to be mobile drag-and-drop
    
    * Run install
    
    * Clean up code and Change link to div in ProjectName
    
    * Divide into HTML5toTouch file and Change to setQueryData
    
    * Create a DemoProjectName file
    
    * Run prettier

--- frontend/src/components/project/ProjectName.jsx
+++ frontend/src/components/project/ProjectName.jsx
@@ -1,17 +1,27 @@
-import { Link } from "react-router-dom"
+import { useRef } from "react"
+import { useNavigate } from "react-router-dom"
 
 import styled, { useTheme } from "styled-components"
 
-import { ifMobile } from "@utils/useScreenType"
+import ProjectNameBox, {
+    NameBox,
+    NameText,
+    TypeText,
+} from "@components/project/ProjectNameBox"
 
 import { getPaletteColor } from "@assets/palettes"
 
 import FeatherIcon from "feather-icons-react"
+import { useDrag, useDrop } from "react-dnd"
 import { useTranslation } from "react-i18next"
 
-const ProjectName = ({ project, demo = false }) => {
-    const { t } = useTranslation("translation", { keyPrefix: "project_list" })
+const ProjectName = ({ project, index, moveProject, dropProject }) => {
+    const { t } = useTranslation(null, { keyPrefix: "project_list" })
     const theme = useTheme()
+
+    const ref = useRef(null)
+    const isInbox = project.type === "inbox"
+    const navigate = useNavigate()
 
     const projectLink =
         project.type === "inbox"
@@ -20,70 +30,83 @@ const ProjectName = ({ project, demo = false }) => {
 
     let name = project.type === "inbox" ? t("inbox") : project.name
 
-    let nameParts = (
-        <Link to={projectLink}>
-            <NameText>{name}</NameText>
-        </Link>
-    )
+    const [{ handlerId }, drop] = useDrop({
+        accept: "Project",
+        collect(monitor) {
+            return {
+                handlerId: monitor.getHandlerId(),
+            }
+        },
+        hover: (item, monitor) => {
+            if (!ref.current) return
 
-    if (demo) {
-        nameParts = <NameText>{name}</NameText>
-    }
+            const dragIndex = item.index
+            const hoverIndex = index
+
+            if (dragIndex === hoverIndex) return
+
+            const hoverBoundingRect = ref.current.getBoundingClientRect()
+            const hoverMiddleY =
+                (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+            const clientOffset = monitor.getClientOffset()
+            const hoverClientY = clientOffset.y - hoverBoundingRect.top
+
+            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return
+            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return
+
+            moveProject(dragIndex, hoverIndex)
+
+            item.index = hoverIndex
+        },
+        drop: (item) => {
+            dropProject()
+            item.index = index
+        },
+    })
+
+    const [{ isDragging }, drag] = useDrag({
+        type: "Project",
+        item: () => {
+            return { project, index }
+        },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    })
+
+    if (!isInbox) drag(drop(ref))
 
     return (
-        <Box>
-            <FlexBox>
+        <ProjectNameBox
+            ref={ref}
+            data-handler-id={handlerId}
+            $isDragging={isDragging}
+            $isInbox={isInbox}>
+            <NameBox>
                 <FeatherIcon
                     icon="circle"
                     fill={getPaletteColor(theme.type, project.color)}
                 />
-                {nameParts}
+                <div onClick={() => navigate(projectLink)} role="link">
+                    <NameText>{name}</NameText>
+                </div>
                 <TypeText>
                     {project.type === "regular" && t("type_regular")}
                     {project.type === "goal" && t("type_goal")}
                 </TypeText>
-            </FlexBox>
-            {!demo && (
-                <TaskCountBox>
-                    <CircleIcon>
-                        <FeatherIcon icon="check" />
-                    </CircleIcon>
-                    <TaskCountText>
-                        {project.completed_task_count}
-                    </TaskCountText>
-                    <CircleIcon />
-                    <TaskCountText>
-                        {project.uncompleted_task_count}
-                    </TaskCountText>
-                </TaskCountBox>
-            )}
-        </Box>
+            </NameBox>
+
+            <TaskCountBox>
+                <CircleIcon>
+                    <FeatherIcon icon="check" />
+                </CircleIcon>
+                <TaskCountText>{project.completed_task_count}</TaskCountText>
+                <CircleIcon />
+                <TaskCountText>{project.uncompleted_task_count}</TaskCountText>
+            </TaskCountBox>
+        </ProjectNameBox>
     )
 }
-
-const Box = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin: 0.8em 0em;
-
-    ${ifMobile} {
-        flex-direction: column;
-        align-items: flex-start;
-    }
-`
-
-const FlexBox = styled.div`
-    display: flex;
-    align-items: center;
-
-    & svg {
-        width: 1.5em;
-        height: 1.5em;
-        stroke: none;
-        top: 0;
-    }
-`
 
 const TaskCountBox = styled.div`
     display: flex;
@@ -98,33 +121,7 @@ const TaskCountBox = styled.div`
     }
 `
 
-const NameText = styled.div`
-    max-width: 10em;
-    font-weight: normal;
-    font-size: 1.25em;
-    margin-left: 0.1em;
-    color: ${(p) => p.theme.textColor};
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    line-height: 1.3em;
-
-    &:hover {
-        color: #ff4a03;
-        cursor: pointer;
-    }
-`
-
-const TypeText = styled.div`
-    font-weight: normal;
-    font-size: 1em;
-    margin-left: 0.6em;
-    margin-top: 0.1em;
-    color: ${(p) => p.theme.secondTextColor};
-`
-
 const TaskCountText = styled.div`
-    font-size: 1em;
     color: ${(p) => p.theme.textColor};
 `
 
@@ -136,17 +133,15 @@ const CircleIcon = styled.div`
     aspect-ratio: 1;
     border-radius: 50%;
     border: 2px solid ${(p) => p.theme.secondTextColor};
-    margin-left: 1em;
-    margin-right: 0.3em;
+    margin: 0 0.3em 0 1em;
 
     & svg {
+        top: 0;
         width: 0.8em;
         height: 0.8em;
         stroke: ${(p) => p.theme.secondTextColor};
         stroke-width: 0.2em;
-        top: 0;
-        margin-top: 0.1rem;
-        margin-right: 0;
+        margin: 0.1rem 0 0;
     }
 `
 
