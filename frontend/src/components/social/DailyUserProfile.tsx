4300febd6410ee4591969b7349610028e09702f4
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-07-17 15:29:17 +0900

    Refactor DailyUserProfile component: enhance loading state handling and improve styling for skeleton elements

--- frontend/src/components/social/DailyUserProfile.tsx
+++ frontend/src/components/social/DailyUserProfile.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 import { useQuery } from "@tanstack/react-query"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 import PageBack from "@components/common/PageBack"
 import PageTitle from "@components/common/PageTitle"
@@ -31,8 +31,11 @@ const DailyUserProfile = ({
         return (
             <Header>
                 <HeaderTexts>
-                    <PageBack defaultTo="/app/social" />
+                    {back && <PageBack defaultTo="/app/social" />}
+                    <UsernameTitle $loading />
+                    {!back && <DisplayNameSkeleton />}
                 </HeaderTexts>
+                <ProfileImgSkeleton />
             </Header>
         )
     }
@@ -71,11 +74,27 @@ const HeaderTexts = styled.div`
     justify-content: left;
 `
 
-const UsernameTitle = styled(PageTitle)`
+const UsernameTitle = styled(PageTitle)<{ $loading?: boolean }>`
     overflow-x: clip;
     text-overflow: ellipsis;
 
     white-space: nowrap;
+
+    ${(p) =>
+        p.$loading &&
+        css`
+            width: 5em;
+            height: 1.5em;
+            border-radius: 7px;
+            background-color: ${(p) => p.theme.skeleton.defaultColor};
+        `}
+`
+
+const DisplayNameSkeleton = styled.div`
+    width: 5em;
+    height: 1em;
+    border-radius: 7px;
+    background-color: ${(p) => p.theme.skeleton.defaultColor};
 `
 
 const ProfileWrapper = styled(Link)`
@@ -85,12 +104,18 @@ const ProfileWrapper = styled(Link)`
 const ProfileImg = styled.img`
     aspect-ratio: 1;
     width: 100%;
-
     border-radius: 100%;
 
     display: flex;
     justify-content: center;
     align-items: center;
+`
+
+const ProfileImgSkeleton = styled.div`
+    width: 4rem;
+    height: 4rem;
+    border-radius: 100%;
+    background-color: ${(p) => p.theme.skeleton.defaultColor};
 `
 
 export default DailyUserProfile
