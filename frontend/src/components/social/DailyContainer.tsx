ff691c71d5ffc8b75a3878e45f6bf7d769355628
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-08-08 14:05:22 +0900

    Edit DailyContainer to update its props type

--- frontend/src/components/social/DailyContainer.tsx
+++ frontend/src/components/social/DailyContainer.tsx
@@ -1,5 +1,9 @@
 import type { Dispatch, SetStateAction } from "react"
 
+import styled from "styled-components"
+
+import FollowButton from "@components/users/FollowButton"
+
 import DailyUserProfile from "./DailyUserProfile"
 import RecordContainer from "./RecordContainer"
 import RemarkContainer from "./RemarkContainer"
@@ -14,23 +18,37 @@ import { DateTime } from "luxon"
 interface DailyContainerProps {
     username: User["username"]
     date: DateTime
+    setDate?: Dispatch<SetStateAction<DateTime>>
+    displayFollowButton?: boolean
+    className?: string
+    standalone?: false
+}
+
+interface DailyContainerPropsStandalone {
+    username: User["username"]
+    date: DateTime
     setDate: Dispatch<SetStateAction<DateTime>>
-    standalone?: boolean
+    displayFollowButton?: boolean
+    className?: string
+    standalone: true
 }
 
 export default function DailyContainer({
     username,
     date,
     setDate,
-    standalone = false,
-}: DailyContainerProps) {
+    displayFollowButton = false,
+    standalone,
+    className,
+}: DailyContainerProps | DailyContainerPropsStandalone) {
     const tz = useClientTimezone()
 
     return (
-        <>
+        <Container className={className}>
             <DailyUserProfile username={username} back={standalone} />
+            {displayFollowButton && <FollowButton username={username} />}
             {standalone && (
-                // TODO: replace DateBar
+                // TODO: edit after typing DateBar
                 <DateBar
                     selectedDate={date}
                     setSelectedDate={(selectedDate: string) =>
@@ -40,6 +58,12 @@ export default function DailyContainer({
             )}
             <RemarkContainer username={username} date={date} />
             <RecordContainer username={username} date={date} />
-        </>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+`
