a21a9d818b6265e3dc3ad7bb2d8b4693250942d7
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-08-10 01:39:31 +0900

    Remove unused legacy social components

--- frontend/src/components/social/common/SimpleProfile.tsx
+++ frontend/src/components/social/common/SimpleProfile.tsx
@@ -1,27 +1,4 @@
-import { Link } from "react-router-dom"
-
 import { styled } from "styled-components"
-
-import { User } from "@api/users.api"
-
-const SimpleProfile = ({ user }: { user: User }) => {
-    return (
-        <Profile to={`/app/users/@${user.username}`}>
-            <SimpleProfileImg $ratio={75} src={user.profile_img} />
-            <Username>@{user.username}</Username>
-        </Profile>
-    )
-}
-
-const Profile = styled(Link)`
-    height: 5em;
-    width: 5em;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
 
 export const SimpleProfileImg = styled.img<{ $ratio?: number }>`
     aspect-ratio: 1;
@@ -33,18 +10,3 @@ export const SimpleProfileImg = styled.img<{ $ratio?: number }>`
     justify-content: center;
     align-items: center;
 `
-
-const Username = styled.div`
-    margin-top: auto;
-    width: 100%;
-
-    overflow-x: clip;
-    text-overflow: ellipsis;
-
-    text-align: center;
-    white-space: nowrap;
-
-    color: ${(p) => p.theme.textColor};
-`
-
-export default SimpleProfile
