482fbe468ad2a94f9c5ace3609d1947923b04905
Bombwhale <20675630+JedBeom@users.noreply.github.com>
2025-10-29 23:35:38 +0900

    Refactor delete_following_for_block and update_follow_count_for_following function signatures for clarity

--- backend/social/signals.py
+++ backend/social/signals.py
@@ -6,18 +6,20 @@ from user_setting.models import UserSetting
 
 
 @receiver(post_save, sender=Block)
-def delete_following_for_block(sender, instance, created, **kwargs):
-    if created:
-        Following.objects.filter(
-            followee=instance.blocker, follower=instance.blockee
-        ).delete()
-        Following.objects.filter(
-            followee=instance.blockee, follower=instance.blocker
-        ).delete()
+def delete_following_for_block(instance: Block, created: bool, **kwargs):
+    if not created:
+        return
+
+    Following.objects.filter(
+        followee=instance.blocker, follower=instance.blockee
+    ).delete()
+    Following.objects.filter(
+        followee=instance.blockee, follower=instance.blocker
+    ).delete()
 
 
 @receiver([post_save, post_delete], sender=Following)
-def update_follow_count_for_following(sender, instance: Following, **kwargs):
+def update_follow_count_for_following(instance: Following, **kwargs):
     instance.follower.followings_count = Following.objects.filter(
         follower=instance.follower, status=Following.ACCEPTED
     ).count()
